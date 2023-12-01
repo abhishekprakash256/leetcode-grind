9fb5f06bd901239ed504936d9094c5c3c1a73594
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-11-30 23:16:43 -0500

    leet code

--- array/leet_code_3sum.py
+++ array/leet_code_3sum.py
@@ -13,8 +13,37 @@ out = [[-1,-1,2],[-1,0,1]]
 class Solution:
 
 	def threeSum(self,nums):
+		"""
+		find the sum that equals to zero
+		"""
 
-		pass
+		nums.sort()
+
+		if len(nums) < 3:
+
+			return False
+
+		for v,i in enumerate(nums):
+
+			l,r = i+1, len(nums) - 1
+
+			while l < r:
+
+				sum = nums[i] + nums[l] + nums[r]
+
+				if sum == 0 :
+
+					return [nums[l],nums[r],nums[i]]
+
+				elif sum < 0:
+
+					r -=1
+
+				else:
+
+					l +=1
+
+		return False
 
 
 
