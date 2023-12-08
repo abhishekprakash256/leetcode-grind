02d10e4a3bd39d4d9dd59cdfd493429a7faf2631
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-07 21:11:35 -0500

    leet code

--- array/leet_code_product_except.py
+++ array/leet_code_product_except.py
@@ -7,4 +7,66 @@ You must write an algorithm that runs in O(n) time and without using the divisio
 
 """
 
-#test 
+#test
+
+arr = [1,2,3,6]
+out = [36,18,12,3]
+
+
+arr2 = [1,0,0,2]
+out2 = [0,0,0,0]
+
+arr3 = [-1,3,2]
+out3 = [6,-2,-3]
+
+
+class Solution(object):
+	def productExceptSelf1(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[int]
+		"""
+
+		prefix = []
+		postfix = []
+		pre_mul, post_mul = 1,1 
+
+
+		for i in nums:
+			
+			pre_mul = pre_mul * i
+			prefix.append(pre_mul)
+
+		for i in nums[::-1]:
+
+			post_mul = post_mul * i
+			postfix.insert(0,post_mul)
+
+
+		mul_arr = []
+		mul_arr.append(postfix[0])
+
+
+		l,m,r = 0,1,2
+
+		while r < len(nums):
+
+			val = prefix[l]*postfix[r]
+			mul_arr.append(val)
+			l +=1
+			m +=1
+			r +=1
+
+		mul_arr.append(postfix[len(postfix)-1])
+
+		return mul_arr
+
+
+if __name__ == "__main__":
+
+	sol = Solution()
+
+	res = sol.productExceptSelf1(arr3)
+
+	print(res)
+		
