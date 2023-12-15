2af0033fff6062581d3f9bb6ed525f958fa09f28
Abhishek <45958856+abhishekprakash256@users.noreply.github.com>
2023-12-15 16:59:12 -0500

    Update leet_code_sum_lst.py

--- strings/leet_code_sum_lst.py
+++ strings/leet_code_sum_lst.py
@@ -1,9 +1,10 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
-print("Hello world")
 
 
 
+print(21%10)
+print(21//10)
 x = 452
 
 #print(x%10) 2
@@ -15,7 +16,7 @@ lst2 = [4,5,6]
 
 def sum_two_num(arr,arr2):
     
-    if len(arr) > len(arr2):
+    if len(arr) < len(arr2):
         arr = arr
         arr = arr2
         
@@ -24,13 +25,23 @@ def sum_two_num(arr,arr2):
         arr, arr2 = arr2, arr
         
     #arr is the greater one 
+    i , j = len(arr) , len(arr2)
+    sum,carry = 0, 0
+    sum_lst = []
     
-    i,j = 0 
-    sum = 0 
-    carry = 0
     
-    while i > 0 :
+    #while i > 0 :
         
-        pass
+        #sum = carry + arr[i] + arr2[i]
+        
+        #while sum != 0:
+            #val = 
+        
+            
+        
+        
+        
+        
+        
         
     
