b4abbec60f4e540518d6423c6471f038dbaa5004
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-18 22:53:30 -0500

    leet code

---
+++ trees/basic_tree.py
@@ -0,0 +1,24 @@
+"""
+make the basic tree
+"""
+
+#make the tree node and connect them
+
+
+class node():
+
+	def __int__(self,val):
+
+		self.left = None
+		self.right = None
+		self.val = val
+
+
+
+#make the tree
+
+root = node()
+node1 = node()
+node2 = node()
+node3 = node()
+node4 = node()
