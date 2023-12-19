3572669a3d6b3b294c1f31f74e90776c24a65cbe
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-18 21:03:01 -0500

    new code

--- strings/leet_code_sum_lst.py
+++ strings/leet_code_sum_lst.py
@@ -15,33 +15,43 @@ lst2 = [4,5,6]
 
 
 def sum_two_num(arr,arr2):
-    
-    if len(arr) < len(arr2):
-        arr = arr
-        arr = arr2
-        
-    #flipping the values
-    else:
-        arr, arr2 = arr2, arr
-        
-    #arr is the greater one 
-    i , j = len(arr) , len(arr2)
-    sum,carry = 0, 0
-    sum_lst = []
-    
-    
-    #while i > 0 :
-        
-        #sum = carry + arr[i] + arr2[i]
-        
-        #while sum != 0:
-            #val = 
-        
-            
-        
-        
-        
-        
-        
-        
-    
+	"""
+	make the program to add two list and have the carry feature as well
+	"""
+
+	#make the arr as always than arr2
+	#arr will be my shorter array
+
+	if len(arr) > len(arr2):
+
+		arr, arr2 = arr2, arr
+
+
+	i,j = len(arr) , len(arr2)
+	sum_arr = []
+	carry, sum = 0, 0
+
+	while i != 0 :
+
+		val = carry + arr[i] + arr2[j]
+
+		if val 
+
+
+
+
+
+
+
+	
+	
+
+		
+			
+		
+		
+		
+		
+		
+		
+	
