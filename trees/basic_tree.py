cc4270076fd959897992180ebdbbdcad04aaffa8
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-19 01:01:37 -0500

    leet code

--- trees/basic_tree.py
+++ trees/basic_tree.py
@@ -5,9 +5,9 @@ make the basic tree
 #make the tree node and connect them
 
 
-class node():
+class node:
 
-	def __int__(self,val):
+	def __init__(self,val):
 
 		self.left = None
 		self.right = None
@@ -17,8 +17,70 @@ class node():
 
 #make the tree
 
-root = node()
-node1 = node()
-node2 = node()
-node3 = node()
-node4 = node()
+root = node(3)
+node1 = node(9)
+node2 = node(20)
+node3 = node(15)
+node4 = node(7)
+
+
+#connect the node 
+root.left = node1
+root.right = node2
+node2.left = node3
+node2.right = node4
+
+
+#wrong solution 
+
+class Solution:
+	def maxDepth(self, root) -> int:
+		"""
+		find the max depth of the tree
+		"""
+
+		if root is None : 
+
+			return 0 
+
+		elif root.left :
+
+			return self.maxDepth(root.left) + 1
+
+		elif root.right:
+
+			return self.maxDepth(root.right) + 1
+
+		else:
+
+			return self.maxDepth(root.left) + self.maxDepth(root.right)
+
+			
+
+	def maxDepth2(self,node):
+		
+		if node is None:
+			return 0
+	 
+		else:
+	 
+			# Compute the depth of each subtree
+			lDepth = self.maxDepth2(node.left)
+			rDepth = self.maxDepth2(node.right)
+	 
+			# Use the larger one
+			if (lDepth > rDepth):
+				return lDepth+1
+			else:
+				return rDepth+1
+
+
+
+if __name__ == '__main__':
+	sol = Solution()
+
+	res = sol.maxDepth2(root)
+
+	print(res)
+
+
