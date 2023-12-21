a79fc4f84c8c2cc6b1a1c67e3fd38dd1bbd19882
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-21 15:38:50 +0000

    leet code

---
+++ trees/leet_code_symetric_tree.py
@@ -0,0 +1,18 @@
+"""
+find the symetric tree
+"""
+
+class node:
+	def __init__(self,val):
+
+		self.left = None
+		self.right = None
+		self.val = val
+
+
+
+#make the tree
+
+root = node(2)
+node1 = node(1)
+node2 = node(3)
