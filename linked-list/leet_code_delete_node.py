99dbaa1ca2c20b82f0df91ffb7184b86c372f694
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-28 20:51:16 +0000

    linked list

--- linked-list/leet_code_delete_node.py
+++ linked-list/leet_code_delete_node.py
@@ -1,2 +1,73 @@
-from linked_list import Node
+"""
+The code to delete the node in linked list 
+"""
+
+#import the linked list nodes
+import linked_list
+
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        temp = linked_list.head
+
+        while True:
+
+            if temp.next is None:
+                break
+
+            #break the node
+            if temp.next.val == node :
+                break_node = temp.next
+                temp.next = temp.next.next
+                break_node.next = None
+                
+
+            temp = temp.next
+
+
+    def printTree(self,head):
+        """
+        The function to print the tree 
+        """
+        temp = head
+
+        while True:
+
+            print(temp.val)
+
+            if temp.next is None:
+                break
+        
+            temp = temp.next
+        
+            
+
+
+
+
+if __name__ == "__main__":
+
+    sol = Solution()
+
+    tree_print = sol.printTree(linked_list.head)
+
+    res = sol.deleteNode(2)
+
+    tree_print2 = sol.printTree(linked_list.head)
+
+
+    print(res) 
+
+
+
+
+
+
+        
 
--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,6 +1,22 @@
 class Node():
     def __init__(self,val):
+        self.val = val
         self.next = None
 
+
+#make the list 
+
+head = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+
+#connect the linked list 
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
 
 
