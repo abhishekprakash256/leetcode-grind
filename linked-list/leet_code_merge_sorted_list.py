398e9ae7bf6719b6675c09fd907b19837d75a647
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-29 22:33:41 -0500

    leet code

--- linked-list/leet_code_merge_sorted_list.py
+++ linked-list/leet_code_merge_sorted_list.py
@@ -8,28 +8,67 @@ Return the head of the merged linked list.
 import linked_list
 
 class Solution:
-    def mergeTwoLists(self, head, head2):
-        """
-        The function to merge the two linked list that are sorted
-        """
+	def mergeTwoLists(self, head, head2):
+		"""
+		The function to merge the two linked list that are sorted
+		"""
 
-        temp, temp2 = head, head2
+		temp, temp2 = head, head2
 
-        while temp:
+		while temp or temp2:
 
-            print(temp.val)
-            temp = temp.next
-        
+			#make the swap 
 
-    
+			#if the value if less 
+			if temp.val < temp2.val :
+
+				temp.next = temp
+				temp2 = temp2.next
+
+
+			# if the value is equal 
+			elif temp.val == temp2.val :
+
+				temp.next = temp2
+				temp2.next = temp
+
+				temp = temp.next
+				temp2 = temp2.next
+
+			# if the value is greater
+			else:
+
+				temp2.next = temp 
+				temp = temp.next
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+	
 
 
 
 
 if __name__ == "__main__":
 
-    sol = Solution()
+	sol = Solution()
 
-    sol.mergeTwoLists(linked_list.head,2)
+	sol.mergeTwoLists(linked_list.head,2)
 
 
