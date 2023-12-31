83cc619dc7d332a76098a2481a4aa581760a6b14
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-31 15:42:42 -0500

    leet code

---
+++ strings/leet_code_longest_substring_without_repeating.py
@@ -0,0 +1,22 @@
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+"""
+
+
+s = "abcabcbb"
+out = 3 
+
+
+s2 = "tttt"
+out2 = 1
+
+
+s3 = ""
+out3 = 0
+
+
+s4 = "abbbbcx"
+out4 = 3 
+
+
+
