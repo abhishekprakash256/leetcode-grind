d1f6170734e83523d9456d18d46e3eb1d38bef99
abhishekprakash256 <abhishekprakash47@gmail.com>
2023-12-31 11:53:57 -0500

    leet code

--- strings/leet_code_longest_pallindrome.py
+++ strings/leet_code_longest_pallindrome.py
@@ -24,11 +24,55 @@ out5 = "tttt"
 
 
 class Solution(object):
-	def longestPalindrome(self, s):
+
+	def checkPallindrome(self,left, right,s):
+		"""
+		The function to check the pallindrome 
+		"""
+
+		while left <= right :
+
+			if s[left] != s[right]:
+				return False
+
+			left +=1
+			right -=1
+
+		return True 
+
+
+
+	def longestPalindrome_brute_force(self, s):
 		"""
 		:type s: str
 		:rtype: str
 		"""
 
 		#cover edge cases 
-		 
+		if len(s) <=1 :
+			return s
+
+		#initilaize the variables 
+		curr = 0
+
+		#start the loop to check 
+		for left in range(len(s)):
+
+			for right in range(left +1, len(s)):
+
+				if self.checkPallindrome(left,right,s) == True:
+
+					curr = max(curr, right - left+1)
+
+		return curr
+
+
+
+
+if __name__ == "__main__":
+	
+	sol = Solution()
+
+	res = sol.longestPalindrome_brute_force(string5) 
+
+	print(res)
