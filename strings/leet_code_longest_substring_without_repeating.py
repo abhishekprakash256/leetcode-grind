a0a5ed2dcb31345536777982e139e2ba2601b609
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-01 17:17:14 -0500

    leet code

--- strings/leet_code_longest_substring_without_repeating.py
+++ strings/leet_code_longest_substring_without_repeating.py
@@ -22,11 +22,16 @@ out4 = 3
 s5 = "au"
 out5 = 2
 
-class Solution(object):
+
+s6 = "asjrgapa"
+out6 = 6
+
+
+class Solution():
 
 	#using the brute force approach
 	#incorrect 
-	def lengthOfLongestSubstring_brute_force(self, s):
+	def lengthOfLongestSubstring_brute_force_incorrect(self, s):
 		"""
 		:type s: str
 		:rtype: int
@@ -49,27 +54,107 @@ class Solution(object):
 		#loop for iteration 
 		for i in range(len(s)):
 
-			mapper[i] = True
+			mapper[s[i]] = True
 
 			for j in range(i+1,len(s)):
 
 				if s[j] not in mapper:
-					
+
+					#debug the code
+
 					mapper[s[j]] = True
 					curr = max(curr, len(mapper))
 
 				else:
 					mapper = {}
 					mapper[s[j]] = True
+					curr = len(mapper)
 
 		return curr 
+
+
+		#works but in O(n^2 timing, 986/987 tests passed)
+	def lengthOfLongestSubstring_brute_force(self,s):
+		"""
+		The function to find the longest string
+		without repeating character 
+		"""
+
+
+		#edge cases
+
+		if len(s) == 0:
+			return 0 
+
+		if len(s) == 1:
+			return 1 
+
+
+		#make the mapper 
+		curr_max = 0
+
+		#start the loop 
+
+
+		for i in range(len(s)):
+
+			mapper = {}
+			mapper[s[i]] = True
+
+			for j in range(i+1,len(s)):
+
+				if s[j] in mapper:
+
+					curr_max = max(curr_max,len(mapper))
+					mapper = {}
+					mapper[s[j]] = True
+
+				else:
+					mapper[s[j]] = True
+					curr_max = max(curr_max,len(mapper))
+
+
+		return curr_max
+
+
+	#optimize solution
+	def lengthOfLongestSubstring(self,s):
+		"""
+		The fucntion to find the lonhest substring 
+		"""
+
+
+		#set the variables
+		char_set = set()
+		res,l = 0,0
+
+		for i in range(len(s)):
+
+			while s[i] in char_set:
+
+				#remove the elements 
+				char_set.remove(s[l])
+				l +=1
+
+
+			char_set.add(s[i])
+
+			res = max(res, i - l + 1)
+
+		return res 
+
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
 
 	sol = Solution()
-	res = sol.lengthOfLongestSubstring_brute_force(s)
+	res = sol.lengthOfLongestSubstring_brute_force(s4)
 
 	print(res)
 
