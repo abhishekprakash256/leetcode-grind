ed6846c820620d4aa0a152f94c71ff7e6a436000
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-02 19:47:23 +0000

    dynamic programming

--- dynamic-programming/dynamic_exp.py
+++ dynamic-programming/dynamic_exp.py
@@ -16,7 +16,17 @@ def fibbo(n):
         return fibbo(n-1) + fibbo(n-2)
 
 
+def fibonacciVal(n):  
+    memo = [0] * (n+1)  
+    memo[0], memo[1] = 0, 1
+      
+    for i in range(2, n+1):    
+        memo[i] = memo[i-1] + memo[i-2]  
+    return memo[n]
 
+
+
+print(fibonacciVal(5))
 #coin change problem 
 
 """
