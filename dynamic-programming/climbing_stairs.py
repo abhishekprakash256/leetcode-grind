f307be1e1ed9f3e0f3d9b83123d8996599fd5330
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-03 20:45:26 -0500

    leet code

--- dynamic-programming/climbing_stairs.py
+++ dynamic-programming/climbing_stairs.py
@@ -4,6 +4,8 @@ one and two steps can be taken at a time
 the steps with same number but different order are consider differnt
 """
 
-
-
+#the problem can be vislauized using a tree soln which the root will be the number of the stps we are in 
+#the edge will be the steps that we took to reach there 
+#using the recursion and depth first search 
+#we can use the DP for the storage of the steps so the steps are not repeacted 
 
