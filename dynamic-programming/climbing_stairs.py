1c884aa5e3408fabbc6a61807c354f79a50a265f
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-03 19:29:34 -0500

    dynamic programming

---
+++ dynamic-programming/climbing_stairs.py
@@ -0,0 +1,9 @@
+"""
+the problem of findindg the number of steps needed to climb the stair case 
+one and two steps can be taken at a time 
+the steps with same number but different order are consider differnt
+"""
+
+
+
+
