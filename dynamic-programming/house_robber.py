7301eab152ba3c67546cb4892aac8733110ff558
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-03 23:59:32 -0500

    leet code

---
+++ dynamic-programming/house_robber.py
@@ -0,0 +1,16 @@
+"""
+leet code 198 
+house robber problem 
+"""
+
+class Solution():
+	def rob(self,nums):
+
+		rob1, rob2 = 0,0
+
+		for n in nums:
+			temp = max(n + rob1, rob2)
+			rob1 = rob2 
+			rob2 = temp
+
+		return rob2
---
+++ dynamic-programming/min_cost_climbing_stairs.py
@@ -0,0 +1,16 @@
+"""
+the min climbing case stairs problem 
+you are given an integer cost[i] is the cost of ith step on the case 
+once you pay the cost, either climb one or steps 
+you can either start from the step with index 0 or index 1 
+return the minimum cost to reach the top of the floor
+"""
+
+
+class Solution():
+	def minCost(self,nums):
+		"""
+		The function to find the min cost of the climbing 
+		"""
+		
+
