380406fab43efd72549d79bd454046015f07cc3a
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-07 21:38:43 -0500

    dynamic prograaming

--- dynamic-programming/pallindromic_sustring.py
+++ dynamic-programming/pallindromic_sustring.py
@@ -3,3 +3,30 @@ find the pallindromic substring in the given string
 the substring contains the value from the string 
 """
 
+class Solution():
+
+	def countSubstrings(self,s):
+		res =0
+
+		for i in range(len(s)):
+
+			l = r = i
+
+			while l >= 0 and r < len(s) and s[l] == s[r]:
+
+				res +=1
+				l -=1
+				r +=1
+
+
+			l = i 
+			r = i +1
+
+			while l >= 0 and r < len(s) and s[l] == s[r]:
+
+				res +=1
+				l -=1
+				r +=1
+
+
+			return res
