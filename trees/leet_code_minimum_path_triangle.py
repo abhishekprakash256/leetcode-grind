b064832c8ba639182d3a3b87daf9f5f80e2b0ef6
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-20 11:38:27 -0500

    traingle code

--- trees/leet_code_minimum_path_triangle.py
+++ trees/leet_code_minimum_path_triangle.py
@@ -2,3 +2,24 @@
 Return the minimum path from the bottom to down
 """
 
+class Solution():
+
+	def minimumTotal(self,triangle):
+		"""
+		The function to find the minimum triangle
+		"""
+
+		dp = [0] * (len(triangle) + 1)
+
+		for row in triangle[::-1]:
+
+			for i,n in enumerate(row):
+
+				dp[i] = n + min(dp[i], dp[i+1])
+
+
+		return dp[0]
+
+
+
+
