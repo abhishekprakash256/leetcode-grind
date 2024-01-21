3eb8bd17c6977329640e1f89252ca67b61de00fd
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-21 16:54:06 -0500

    code

---
+++ trees/leet_code_sum_root.py
@@ -0,0 +1,28 @@
+"""
+leet code find the sum of the root ot leaf node
+find the sum of the root node from the left and right 
+we add and make a two digit number then we add them together 
+Example 
+-----
+
+		8
+	   / \
+	  7   4
+
+87 + 84 = 171
+"""
+
+
+"""
+The methedelogy as follows - 
+
+
+#base case 
+if not node: 
+	return 0 
+
+
+
+
+"""
+
