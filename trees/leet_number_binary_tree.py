1ddf85459acf84297b41245a3d4f885da666bf44
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-21 20:22:09 -0500

    code

---
+++ trees/leet_number_binary_tree.py
@@ -0,0 +1,10 @@
+"""
+FInd the number of the binary tree that can be formed from the number of given node
+"""
+
+class Solution():
+
+	def numTrees(self,nums):
+		"""
+		The function to find the number of trees that can be formed 
+		"""
