afb4148ecd1e1e49af9aaafd3a9ec7033fe1de30
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-01-27 14:22:57 -0500

    code

---
+++ array/leet_code_missing_positive.py
@@ -0,0 +1,27 @@
+"""
+find the missing postive in the array 
+smallest positive array 
+"""
+
+
+"""
+find the missing postive integer
+
+
+
+
+
+"""
+
+
+
+
+class Solution():
+
+	def firstMissingPositive(self, nums) -> int:
+		"""
+		The function to find the minimum missing 
+		positive integer in the array 
+		"""
+
+		pass
