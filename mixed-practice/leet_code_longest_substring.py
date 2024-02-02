2d38c089d0ccb77b594c4b791b82080ae5301c7a
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-02 21:10:42 +0000

    code

--- mixed-practice/leet_code_longest_substring.py
+++ mixed-practice/leet_code_longest_substring.py
@@ -22,8 +22,32 @@ set to store my unique elements
 
 l = 0 
 r = l+=1
-
-while r < len(s):
-
-    s.append()
+queue = set()
+	
 """
+
+
+class Solution:
+	def lengthOfLongestSubstring(self, s):
+		"""
+		The function to find the longest pallindrome sustring 
+		"""
+
+		char_set = ()
+		res, r = 0,0 
+
+		for i in range(len(s)):
+
+			while s[i] in char_set:
+				char_set.remove(s[l])
+
+				l+=1
+			
+			char_set.add(s[i])
+
+			res = max(res,i - l + 1)
+		
+		return res
+
+
+
