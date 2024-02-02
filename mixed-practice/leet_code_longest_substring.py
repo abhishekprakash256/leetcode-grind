72c60edb6f4ae794fc6af5495821b31524f596b5
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-02 20:56:45 +0000

    code

---
+++ mixed-practice/leet_code_longest_substring.py
@@ -0,0 +1,29 @@
+"""
+leet code the longest pallindrome substring 
+"""
+
+s = "abcabcbb"
+out = 3
+
+
+s2 = "bbbbb"
+out2 = 1
+
+s3 = "pwwkew"
+out3 = 3 
+
+
+"""
+Algo 
+
+two pointer approach 
+sliding window 
+set to store my unique elements 
+
+l = 0 
+r = l+=1
+
+while r < len(s):
+
+    s.append()
+"""
