78949a2c3fca7a146153888a01bce1ae58517d87
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-02 21:33:53 +0000

    code

---
+++ mixed-practice/leet_code_3sum.py
@@ -0,0 +1,20 @@
+"""
+The program to find the 3 sum of the list 
+such that i != j, i != k, and j != k, and
+and sum is 0 
+"""
+
+nums = [-1,0,1,2,-1,-4]
+out = [[-1,-1,2],[-1,0,1]]
+
+nums2 = [0,1,1]
+out2 = []
+
+
+class Solution():
+	def threeSum(self, nums):
+		"""
+		The program to make the 3 sum 
+
+		"""
+		
