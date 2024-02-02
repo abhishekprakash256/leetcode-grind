57792629ba5c184be5e41c0f3c2a324cc4250454
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-02 21:54:06 +0000

    code

--- mixed-practice/leet_code_3sum.py
+++ mixed-practice/leet_code_3sum.py
@@ -15,6 +15,44 @@ class Solution():
 	def threeSum(self, nums):
 		"""
 		The program to make the 3 sum 
-
 		"""
+		#sort the array 
+		nums.sort()
+		k = len(nums) - 1
+		res_lst = []
+
+
+		for i in nums: 
+
+			j = i + 1 
+			sum = nums[i] + nums[j] + nums[k]
+
+			#start the loop
+			while l <= r:
+
+				if sum  == 0 and i != j and i= k and j = k:
+					res_lst.append([nums[i],nums[j],nums[k]])
+
+				elif sum > 0:
+					l -=1
+
+				else:
+					r +=1
+
+		
+		return res_lst
+
+
+sol = Solution()
+res = sol.threeSum(nums)
+
+print(res)
+
+
+
+
+
+
+
+
 		
