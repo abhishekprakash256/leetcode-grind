2303cab727cd53be7efb20a48e6b5bd6f4664bff
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-07 20:58:42 -0500

    code

--- trees/leet_code_symetric_tree2.py
+++ trees/leet_code_symetric_tree2.py
@@ -2,3 +2,35 @@
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 """
 
+"""
+algo -- 
+
+recursion 
+
+base :
+if node.left is None and node.right is None:
+	return True
+
+
+def helper(node):
+
+	return node.val
+
+
+
+def isSymetric(self,node):
+	
+	#base case 
+	if node.left is None and node.right is None:
+		return True
+
+
+	
+	left_val = self.helper(node.left) if node.left else None 
+	right_val = self.helper(node.right) if node.right else None 
+
+	if left_val == right_val:
+		return True 
+
+	else :
+		return False
