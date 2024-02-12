c6caebea39cda9e6c7eb97a0f9da35db28124241
Abhishek <45958856+abhishekprakash256@users.noreply.github.com>
2024-02-12 16:03:59 -0500

    Create leet_code_tree_zig_zag.py
    new code

---
+++ trees/leet_code_tree_zig_zag.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def helper(self,node,res_lst,flip,tempr_lst= []):
+        """
+        The helper function for traversal
+        """
+        if not node:
+            tempr_lst = []
+            return None
+        
+        
+        if flip is True:
+            
+            tempr_lst.append(node.val)
+            self.helper(node.left,res_lst,flip,tempr_lst)
+            self.helper(node.right,res_lst,flip,tempr_lst)
+            res_lst.append(tempr_lst)
+            flip = False
+            
+            
+        else:
+            
+            tempr_lst.append(node.val)
+            self.helper(node.right,res_lst,flip,tempr_lst)
+            self.helper(node.left,res_lst,flip,tempr_lst)
+            res_lst.append(tempr_lst)
+            flip = True
+            
+            
+        
+
+            
+    
+    def zigzagLevelOrder(self, node: Optional[TreeNode]) -> List[List[int]]:
+        
+        res_lst = []
+        flip = True
+        
+        if not node:
+            return None
+        
+        self.helper(node,res_lst,flip)
+        
+        return res_lst
+        
+        
+        
+        
