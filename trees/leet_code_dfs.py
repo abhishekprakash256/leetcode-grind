b5558ad3ea480df7d62f54b1d012f21fc2f7a7e3
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-13 23:05:22 -0500

    code

--- trees/leet_code_dfs.py
+++ trees/leet_code_dfs.py
@@ -1,3 +1,70 @@
 """
 the dfs traversal of the tree using the iterative and recursive approach 
 """
+
+# Example binary tree
+#          1
+#        /   \
+#       2     3
+#      / \   / \
+#     4   5 6   7
+class TreeNode:
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+		
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+
+
+
+class Solution():
+
+	def dfs_iterative(self,node):
+		"""
+		The dfs iterative appraoch of the tree
+		"""
+
+		#make the initial list 
+		res_lst = []
+
+		if not node:
+			return res_lst
+
+
+		#make the queue
+		queue = []
+		queue.append(node)
+
+		while queue:
+
+			node = queue.pop()
+
+			if node:
+				res_lst.append(node.val)
+
+			if node.right:
+				queue.append(node.right)
+
+			if node.left:
+				queue.append(node.left)
+
+		return res_lst
+
+
+
+if __name__ == '__main__':
+	sol = Solution()
+
+	res_iterative = sol.dfs_iterative(root)
+
+	print(res_iterative)
+
