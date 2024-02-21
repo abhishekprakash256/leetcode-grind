e13db01d02f31e892fd8f39951dd2ba383fe87fa
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-21 00:08:53 -0500

    code

--- dynamic-programming/leet_code_jumps.py
+++ dynamic-programming/leet_code_jumps.py
@@ -27,9 +27,54 @@ basic approach recursion --
 
 
 
+class Solution():
+
+	def helper(self,i,nums):
+		"""
+		The function to find the jumps reaching end of list
+
+		"""
+		#base case 
+		if i == len(nums) - 1:
+			return True
+
+		for jumps in range(i+1):
+
+			self.helper(jumps,nums)
+
+		return False
+
+
+	def canJump(self,nums):
+		"""
+		The main function 
+		"""
+
+		#constraints 
+		if not nums:
+			return None
+
+
+		res = False
+
+		for i in nums:
+
+			res = self.helper(i,nums)
+
+			if res == True:
+				return True
+
+		return res
 
 
 
+
+sol = Solution()
+
+
+res = sol.canJump(nums2)
+
+print(res)
 
 
 
