8da35aff7b7e2bd3296fb48d6973b667befd7177
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-02-20 22:54:29 -0500

    code

--- dynamic-programming/leet_code_jumps.py
+++ dynamic-programming/leet_code_jumps.py
@@ -20,39 +20,25 @@ algo --
 basic approach recursion -- 
 
 
-def helper(i,nums):
-
-	if i == len(nums) - 1:
-		return True 
-
-	else:
-		return False
-
-	for jumps in range(n+1):
-		
-		self.helper(jumps,nums)
-
-def canJump(self,nums):
-
-	if not nums:
-		return None 
-	
-	res = False
-
-	for i in nums:
-
-		if res == True:
-			return True 
-
-		res = self.helper(i,nums)
-
-
-	return res
-
 
 
 """
 
-class Solution():
 
-	def helper(self,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
