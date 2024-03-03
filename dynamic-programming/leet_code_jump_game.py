9c5777915d3aa5e1f0b9cbca42182d2e1bc0535d
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-03-02 19:35:22 -0500

    code

--- dynamic-programming/leet_code_jump_game.py
+++ dynamic-programming/leet_code_jump_game.py
@@ -29,39 +29,21 @@ make the algo --
 
 class Solution():
 
-	def canJump_brute_force_incorrect(self,nums):
+	def canJump(self,nums):
 		"""
 		The function to find the brute force soln of can jump 
 		"""
 
-		if len(nums) == 1:
+		goal = len(nums) - 1
 
-			if nums[0] == 0 or nums[0] ==1 :
-				return True
+		#start the loop 
 
-		for i in range(len(nums) - 1):
+		for i in range(len(nums)-1,-1,-1):
 
-			for j in range(1,nums[i]+1):
+			if i + nums[i] >= goal :
+				goal = i 
 
-				#print(i+j)
-
-				if i + j == len(nums)- 1:
-					return True 
-
-
-		return False
-
-
-	def canJump_dp(self,nums):
-		"""
-		The function to find the jumps possible
-		"""
-
-		#base casese
-		if len(nums) == 1:
-
-			if nums[0] == 0 or nums[0] ==1 :
-				return True
+		return True if goal == 0 else False
 
 
 		
@@ -72,4 +54,4 @@ if __name__ == '__main__':
 	
 	sol = Solution()
 
-	print(sol.canJump_brute_force(nums2))
+	print(sol.canJump(nums2))
