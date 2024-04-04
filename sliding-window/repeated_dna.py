6b7d566526151e0da4bb5b691c16ce3ab881b905
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-04 00:21:03 -0400

    code

--- sliding-window/repeated_dna.py
+++ sliding-window/repeated_dna.py
@@ -2,3 +2,34 @@
 the repeated dna problem
 """
 
+
+def find_repeated_sequences(s, k):
+    """
+    the function to find the repeated sequences of given length 
+    """
+
+    #vars 
+    mapper = {}
+    left = 0 
+    right = k - 1 
+
+    #star the iter loop 
+    while right < len(s):
+
+        if s[left:right+1] not in mapper:
+            mapper[s[left:right + 1]] = 1
+        
+        else:
+            mapper[s[left:right + 1]] +=1
+        
+        left+=1
+        right+=1
+
+    return {key for key in mapper if mapper[key] > 1}
+    
+    #get the final results 
+    
+        
+
+
+
