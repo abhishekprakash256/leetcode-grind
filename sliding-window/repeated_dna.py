10144cd3de713ace81a67c6f8216cd5f324435dd
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-05 07:26:15 -0400

    code

--- sliding-window/repeated_dna.py
+++ sliding-window/repeated_dna.py
@@ -2,34 +2,55 @@
 the repeated dna problem
 """
 
+class Solution():
+	def find_repeated_sequences(s, k):
+	    """
+	    the function to find the repeated sequences of given length 
+	    """
 
-def find_repeated_sequences(s, k):
-    """
-    the function to find the repeated sequences of given length 
-    """
+	    #vars 
+	    mapper = {}
+	    left = 0 
+	    right = k - 1 
 
-    #vars 
-    mapper = {}
-    left = 0 
-    right = k - 1 
+	    #star the iter loop 
+	    while right < len(s):
 
-    #star the iter loop 
-    while right < len(s):
+	        if s[left:right+1] not in mapper:
+	            mapper[s[left:right + 1]] = 1
+	        
+	        else:
+	            mapper[s[left:right + 1]] +=1
+	        
+	        left+=1
+	        right+=1
 
-        if s[left:right+1] not in mapper:
-            mapper[s[left:right + 1]] = 1
-        
-        else:
-            mapper[s[left:right + 1]] +=1
-        
-        left+=1
-        right+=1
+	    return {key for key in mapper if mapper[key] > 1}
+	    
+	    #get the final results 
+	    
+	def find_repeated_sequences_optim(s, k):
+	    """
+	    The hash set code 
+	    """
 
-    return {key for key in mapper if mapper[key] > 1}
-    
-    #get the final results 
-    
-        
+	    seen,res = set(),set()
+
+	    for i in range(len(s)-k):
+
+	        subs = s[i:i+k]
+
+	        if subs in seen:
+	            res.add(subs)
+
+	        seen.add(subs)
+
+	    return list(res)
+	 
+
+
+
+
 
 
 
