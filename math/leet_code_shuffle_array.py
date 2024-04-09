dfe5c1b2f425fa273b3cd0e84e3db3ca7abbabfc
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-09 07:49:40 -0400

    code

--- math/leet_code_shuffle_array.py
+++ math/leet_code_shuffle_array.py
@@ -11,3 +11,32 @@ Implement the Solution class:
 
 
 
+
+#the solution of the random shuffle 
+
+class Solution():
+
+    def __init__(self,nums):
+        self.array = nums
+        self.original = list(nums)
+
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+
+        for i in range(len(self.array)):
+            idx = random.randrange(i,len(self.array))
+
+            self.array[i],self.array[idx] = self.array[idx], self.array[i]
+
+        return self.array
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
