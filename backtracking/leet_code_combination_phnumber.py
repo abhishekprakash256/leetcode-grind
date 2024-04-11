cc44f2efbf37dfba69b6edfb25b6df831a50eecd
Abhishek <abhishekprakash47@gmail.com>
2024-04-11 17:44:54 -0400

    code

--- backtracking/leet_code_combination_phnumber.py
+++ backtracking/leet_code_combination_phnumber.py
@@ -9,6 +9,10 @@ A mapping of digits to letters (just like on the telephone buttons) is given bel
 digits = "23"
 out = ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+
+
 The mapping 
 
 mapper = {2:['a','b','c'], 3:['d','e','f'],4:['g','h','i'],5:['j','k','l'], 6:['m','n','o'], 7:['p','q','r','s'],8:['t','u','v'],9:['w','x','y','z']}
@@ -27,3 +31,38 @@ if len(digits) == 0:
 
 
 """
+
+
+class solution():
+    def letter_combinations(digits):
+        if not digits:
+            return []
+
+        # Define the mapping of digits to letters
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        def backtrack(index, path):
+            if index == len(digits):
+                combinations.append(path)
+                return
+            
+            for letter in mapping[digits[index]]:
+                backtrack(index + 1, path + letter)
+
+        combinations = []
+        backtrack(0, '')
+        return combinations
+
+# Example usage:
+sol = solution()
+digits = "23"
+print(sol.letter_combinations(digits))  # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
