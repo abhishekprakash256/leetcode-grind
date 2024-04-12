5158f2e811db03861a9ef245469b9905083e8b15
Abhishek <abhishekprakash47@gmail.com>
2024-04-12 14:26:52 -0400

    code

--- backtracking/leet_code_generate_paranthesis.py
+++ backtracking/leet_code_generate_paranthesis.py
@@ -10,4 +10,36 @@ Output: ["()"]
 
 """
 
+class Solution():
+    def __init__(self):
+        self.res = []
+
+    def backtrack(self,s, left,right):
+        """
+        The funciton to make the paranthesis
+        """
+        #base case 
+        if len(s) == 2 *self.n:
+            self.res.append(s)
+            return
+        
+        #two cases
+        if left < self.n:
+            self.backtrack(s + "(",left + 1,right)
+        
+        if right < left:
+            self.backtrack(s + ")",left, right + 1 )
+        
+
+
+
+    def generateParenthesis(self,n):
+        self.n = n 
+        """
+        The funciton to generate the paranthesis 
+        """
+        
+        self.backtrack("",0,0)
+
+        return self.res
 
