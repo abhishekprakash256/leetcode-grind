6c603856aeec58ac685a99426a7a4b56e6c42a31
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-04-17 00:52:30 -0400

    code

--- sorting-searching/leet_code_top_k_elements.py
+++ sorting-searching/leet_code_top_k_elements.py
@@ -5,5 +5,30 @@ Given an integer array nums and an integer k, return the k most frequent element
 nums = [1,1,1,2,2,3]
 k = 2
 
+out = [1,2]
+
+
+O(nlogn)
+
+
+
+[9,8,1,1,1,3,4,4,5]
+
+[1,4]
+
+[1,1,1,3,4,4,5,8,9]
+
+[1,4]
+
+
+[1,1,1,2,2,1,1,4,5,5,5]
+
+[1,1,1,1,1,1,2,2,5,5,5]
+
+[1,5]
+
+
+
+
 
 """
