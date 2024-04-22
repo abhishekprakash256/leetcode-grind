c26d66a52a68552a14723ea529534607298a11a9
Abhishek <abhishekprakash47@gmail.com>
2024-04-22 17:09:44 -0400

    code

--- sorting-searching/leet_code_search_range.py
+++ sorting-searching/leet_code_search_range.py
@@ -85,88 +85,96 @@ nums = [5,7,7,10,10]
 target = 7
 
 nums2 = [2,2] 
-target2 = 2
+target2 = 3
 
 
 
 # soln 
 
 class Solution2:
-    def searchRange(self, nums, target):
-        """
-        Find the target range in the nums array 
-        """
-        res = [-1, -1]
+	def searchRange(self, nums, target):
+		"""
+		Find the target range in the nums array 
+		"""
+		res = [-1, -1]
 
-        # Base case
-        if not nums:
-            return res
+		# Base case
+		if not nums:
+			return res
 
-        # Binary search to find the leftmost index of the target
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        if nums[left] != target:
-            return res
-        res[0] = left
+		# Binary search to find the leftmost index of the target
+		left, right = 0, len(nums) - 1
+		while left < right:
+			mid = left + (right - left) // 2
+			if nums[mid] < target:
+				left = mid + 1
+			else:
+				right = mid
+		if nums[left] != target:
+			return res
+		res[0] = left
 
-        # Binary search to find the rightmost index of the target
-        right = len(nums) - 1
-        while left < right:
-            mid = left + (right - left) // 2 + 1  # Adjusted mid calculation
-            if nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid
-        res[1] = right
+		# Binary search to find the rightmost index of the target
+		right = len(nums) - 1
+		while left < right:
+			mid = left + (right - left) // 2 + 1  # Adjusted mid calculation
+			if nums[mid] > target:
+				right = mid - 1
+			else:
+				left = mid
+		res[1] = right
 
-        return res
+		return res
 
 
 #going in infinite loop
 
 class Solution():
 
-    def searchRange(self, nums, target):
-        """
-        The function to find the range in the given target
-        """
+	def searchRange(self, nums, target):
+		"""
+		The function to find the range in the given target
+		"""
 
-        res = [-1, -1]
+		res = [-1, -1]
 
-        if not nums:
-            return res
+		if not nums:
+			return res
 
-        l, r = 0, len(nums) - 1
+		if len(nums) == 1:
+			
+			if nums[0] == target:
+				return [0,0]
+			else:
+				return res
 
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] < target:
-                l = m + 1
-            else:
-                r = m - 1
+		l, r = 0, len(nums) - 1
 
-        if nums[l] != target:
-            return res
+		while l <= r:
+			m = (l + r) // 2
+			if nums[m] < target:
+				l = m + 1
+			else:
+				r = m - 1
+		
+		if l < 0 or l > len(nums) - 1 or nums[l]!= target:
+			return res
+	
 
-        res[0] = l
+		res[0] = l
 
-        l, r = 0, len(nums) - 1
+		l, r = 0, len(nums) - 1
 
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] <= target:  # Adjusted condition for finding the rightmost index
-                l = m + 1
-            else:
-                r = m - 1
+		while l <= r:
+			m = (l + r) // 2
+			if nums[m] <= target:  # Adjusted condition for finding the rightmost index
+				l = m + 1
+			else:
+				r = m - 1
 
-        res[1] = r
+		res[1] = r
 
-        return res
+		return res
 
 
 
