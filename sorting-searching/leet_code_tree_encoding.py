9237bf2d86336d0a7202ea7690f75bb9af981ea5
Abhishek <abhishekprakash47@gmail.com>
2024-04-22 17:32:18 -0400

    code

--- sorting-searching/leet_code_tree_encoding.py
+++ sorting-searching/leet_code_tree_encoding.py
@@ -7,3 +7,33 @@ Design an algorithm to serialize and deserialize a binary tree. There is no rest
 that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
 """
 
+class TreeNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+
+#make the tree 
+
+root = TreeNode(1)
+
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.right.right = TreeNode(5)
+root.right.left = TreeNode(4)
+
+
+#print the tree
+
+def tree_printer(node):
+
+	if node:
+		print(node.val)
+
+		tree_printer(node.left)
+		tree_printer(node.right)
+
+
+
+print(tree_printer(root))
