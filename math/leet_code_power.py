981129a04dad0658760c113ebd97f13b3e24686d
Abhishek <abhishekprakash47@gmail.com>
2024-04-24 16:52:34 -0400

    code

--- math/leet_code_power.py
+++ math/leet_code_power.py
@@ -84,7 +84,16 @@ class Solution():
 		return res
 
 
-		
+
+	def myPow(self, x: float, n: int) -> float:
+		if n == 0:
+			return 1
+		elif n < 0:
+			return 1 / (x * self.myPow(x, -n - 1))
+		elif n % 2 == 0:
+			a = self.myPow(x, n // 2)
+			return a * a
+		return x * self.myPow(x, n - 1)	
 
 
 
