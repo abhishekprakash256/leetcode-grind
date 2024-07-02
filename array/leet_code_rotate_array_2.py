8764ed01f25820e49f7690a8b289dc49b140d437
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-07-02 00:13:48 -0400

    rotate array code

---
+++ array/leet_code_rotate_array_2.py
@@ -0,0 +1,131 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+
+
+"""
+
+"""
+visualization 
+
+1. approach 
+brute force 
+
+pop()
+insert at zero 
+
+
+2nd one 
+swap use two pointers 
+
+if k == len(nums):
+	return nums 
+
+if k < len(nums):
+	l = 0 
+	r =  len(nums) - k 
+
+	keep swap till k times 
+
+elif k > len(nums):
+	res = len(nums) % k 
+	l = 0 
+	r = len(nums) - k 
+
+
+
+
+"""
+
+
+class Solution:
+	def rotate_brurte_force(self, nums: List[int], k: int) -> None:
+		"""
+		Do not return anything, modify nums in-place instead.
+		"""
+
+		for i in range(k):
+
+			res = nums.pop()
+			nums.insert(0,res)
+
+
+	def rotate(self, nums: List[int], k: int) -> None:
+		"""
+		Do not return anything, modify nums in-place instead.
+		"""
+
+		#edge case
+		length = len(nums) 
+		l = 0 
+		count = 0 
+
+		if k < length:
+
+			r = length - k  + 1
+
+
+		elif k > length:
+			res = length % k
+			r = length - res
+
+		print(l,r)
+
+
+		while r < length:
+
+			nums[l], nums[r] = nums[r] , nums[l]
+			l +=1 
+			r +=1
+			count +=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
