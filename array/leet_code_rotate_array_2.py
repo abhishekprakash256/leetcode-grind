f10c0e69b67e36ac995eb2803b0b54e3f87465d4
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-07-02 23:54:30 -0400

    code

--- array/leet_code_rotate_array_2.py
+++ array/leet_code_rotate_array_2.py
@@ -1,4 +1,5 @@
 """
+
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
 Input: nums = [1,2,3,4,5,6,7], k = 3
@@ -9,6 +10,7 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
 
+<<<<<<< HEAD
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
 Explanation: 
@@ -51,10 +53,27 @@ elif k > len(nums):
 	l = 0 
 	r = len(nums) - k 
 
+=======
+Constraints:
+
+    1 <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 105
+
+
+Follow up:
+
+    Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+    Could you do it in-place with O(1) extra space?
+
+
+
+
 
 
 
 """
+
 
 
 class Solution:
@@ -97,16 +116,6 @@ class Solution:
 			l +=1 
 			r +=1
 			count +=1
-
-
-
-
-
-
-
-
-
-
 
 
 
