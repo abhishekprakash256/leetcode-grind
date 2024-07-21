eb7127b1436bad06f72c46177803ed47f9e696ed
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-07-21 18:23:31 -0400

    array code

---
+++ array/leet_code_move_zeros_2.py
@@ -0,0 +1,25 @@
+"""
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+
+Input: nums = [0]
+Output: [0]
+"""
+
+
+
+"""
+#constarints 
+
+len(nums) == 0 or len(nums) == 1
+	return empty or nums 
+
+
+
+"""
