ecf46aa3319dee6f8a28ef55fcdd580431e9f5c1
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-07-24 23:48:47 -0400

    anagram

--- strings/leet_code_anagram.py
+++ strings/leet_code_anagram.py
@@ -14,10 +14,86 @@ Output: false
 
 
 """
+1. brute force 
+
+use a hash map of frequnecy table 
+iterate all and compare and add to hash map 
+
+do as that for second one too
+
+matching both hashmap 
+if freq matches then gives true 
+else false 
+
+
+
 
 
 
 
 """
+
+
+
+class Solution:
+	def isAnagram_bruteforce(self, s: str, t: str) -> bool:
+		"""
+		The function to check for the anagram
+		leetcode passed
+		"""
+
+		#make two hashmaps 
+		mapper_one = {}
+		mapper_two = {}
+
+
+		#start the first loop for freq table 
+
+		for i in s:
+
+			if i in mapper_one :
+				mapper_one[i] = mapper_one[i] + 1 
+
+			else:
+				mapper_one[i] = 1
+
+
+		#start the first loop for freq table 
+
+		for i in t:
+
+			if i in mapper_two :
+				mapper_two[i] = mapper_two[i] + 1 
+
+			else:
+				mapper_two[i] = 1
+
+
+
+		#compare the hash map
+
+		if mapper_one == mapper_two:
+			return True
+
+		else:
+			return False
+
+
+	def isAnagram(self, s: str, t: str) -> bool:
+		"""
+		The function to check for the anagram
+		"""
+		
+
+
+
+
+
+
+
+
+
+
+
 
 
