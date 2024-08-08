b6cee40d82fd854d7493757c8d8c4946a51d7699
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-07 22:42:58 -0400

    strings

--- strings/leet_code_common_prefix.py
+++ strings/leet_code_common_prefix.py
@@ -10,7 +10,80 @@ Output: "fl"
 
 
 """
+1.Approach
+
+#base case 
+if len(strs) == 1:
+	return strs[0]
+
+
+common_prefix = strs[0]
+
+
+for i in range(1,len(strs)):
+
+	length = min(len(common_preifx),len(strs[i])):
+	
+	for idx in range(0,length):
+
+		if common_prefix[idx] != strs[idx]:
+			common_prefix = common_prefix[0:strs]
+
+return common_prefix
+
+
+2.Approach
+
+#base case 
+if len(strs) == 1:
+	return strs[0]
+
+
+common_prefix = ""
 
 
 
 """
+
+
+class Solution:
+	def longestCommonPrefix(self, strs):
+		"""
+		The function to find the longes common prefix 
+		"""
+
+		#base case 
+		if len(strs) == 1:
+			return strs
+
+
+		#make the common prefix 
+		common_prefix = strs[0]
+
+		for i in range(1,len(strs)):
+
+			length = min(len(strs[i]),common_prefix)
+
+			for idx in range(0,length):
+
+				if common_prefix[idx] != strs[i][idx]:
+					common_prefix = common_prefix[0:idx]
+
+
+		return common_prefix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
