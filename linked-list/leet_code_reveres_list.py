0e5cfe843947bd2066c71b4062ff29157e1755b7
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-10 23:40:54 -0400

    revesed code

--- linked-list/leet_code_reveres_list.py
+++ linked-list/leet_code_reveres_list.py
@@ -8,7 +8,40 @@ Output: [5,4,3,2,1]
 
 """
 
+
+
 """
+the soln 
+
+--
+
+head = [1,2,3,4,5]
+
+1->2->3->4->5
+
+5->4->3->2->1
+
+brute force 
+
+#edge case 
+temp = root
+
+if not temp :
+
+	retun []
+
+
+1. append all in a list 
+2 rev that list 
+3. make a new list by iteration and keep adding elemnts 
+
+O(n), O(n)
+
+
+optimal soln -  
+
+1. 
+
 
 
 """
@@ -26,9 +59,51 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]):
-    	"""
+	def reverseList(self, head: Optional[ListNode]):
+		"""
+		Rewverse the link list
+		works and passed on leetcode
 
-    	"""
-    	pass
-    	
+		"""
+		#base case
+
+		if not head:
+			return None
+
+		prev = None
+		curr = head
+
+		while curr:
+
+			#swap the pointers 
+			next_node = curr.next
+			curr.next = prev
+			prev = curr
+			curr = next_node
+
+		return prev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
