2f70262e9825299acd249c663b3e4751c7811940
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-18 20:23:54 -0400

    tree code

---
+++ trees/leet_code_inorder_traversal_2.py
@@ -0,0 +1,61 @@
+"""
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+in dfs
+left -> root -> right 
+"""
+
+"""
+approach 
+
+dfs using the inorder 
+recursion 
+
+make a list and pass the node to the helper function 
+
+two function or method 
+
+def helper(self,node_lst,node)
+def inorderTraversal(self,root)
+
+
+base case 
+if not node:
+	return node
+
+
+
+"""
+
+
+class Solution():
+
+	def helper_dfs(self,node,node_lst):
+		"""
+		The helper function to make the dfs traversaL
+		"""
+
+		if node:
+
+			self.helper_dfs(node.left,node_lst):
+
+			node_lst.append(node.val)
+
+			self.helper_dfs(node.right,node_lst)
+
+	def inorderTraversal(self,root):
+		"""
+		The main traversal function
+		the code passed leetcode
+		"""
+
+		#base case
+		if not root:
+			return None
+
+		node_lst = []
+
+		self.helper_dfs(root,node_lst)
+
+		return node_lst
