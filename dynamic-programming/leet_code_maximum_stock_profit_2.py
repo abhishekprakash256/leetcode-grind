73b35ee10cdf5a08b0fba0ca4374b21025858951
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-08-29 01:07:35 -0400

    code

--- dynamic-programming/leet_code_maximum_stock_profit_2.py
+++ dynamic-programming/leet_code_maximum_stock_profit_2.py
@@ -78,6 +78,28 @@ class Solution(object):
 
 
 
+class Solution2:
+
+    def maxProfit(self, prices):
+    	"""
+    	optimizex solution
+
+    	"""
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
+
+
+
+
+
+
+
 
 
 
