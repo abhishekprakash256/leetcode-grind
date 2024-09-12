d1e46c8741581e4879f8d854f00e511512ef80ac
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-09-12 16:30:59 -0400

    code

--- dynamic-programming/leet_code_can_jump.py
+++ dynamic-programming/leet_code_can_jump.py
@@ -7,6 +7,7 @@ class Solution():
 	def canJump(self,nums):
 		"""
 		The function to find the brute force soln of can jump 
+        The code works in leetcode
 		"""
 
 		gas = 0 
@@ -16,9 +17,33 @@ class Solution():
 			if gas < 0 :
 				return False
 		
-			elif n > gas:
+			elif gas < n:
 				gas = n 
 			
 			gas -=1
 		
 		return True
+	
+
+class Solution2():
+    def canJump(self, nums: List[int]) -> bool:
+        """
+        The function to find the jump possible
+        The code works in leetcode
+        
+        """
+        
+        gas = 0 
+        
+        for i in nums:
+            
+            if gas < 0:
+                return False
+            
+            elif gas < i:
+                gas = i
+            
+            gas -= 1
+        
+        return True
+        
