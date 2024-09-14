7c023523850180d18e279ffcd21f63a3c8e02a90
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-09-14 16:30:54 -0400

    code

--- dynamic-programming/leet_code_unique_paths.py
+++ dynamic-programming/leet_code_unique_paths.py
@@ -25,38 +25,33 @@ class Solution():
 
 	def helper(self,i,j,m,n):
 		"""
-		The helper function to do the dfs
+		The helper function for dfs
 		"""
 
 		#base case 
-		if i == m - 1 and j == n - 1 :
-			return True
-
-		#base case out of bound 
-		if i > m - 1 or j > n -1 :
-			return False
-
-
-		#make the function call 
-		self.helper(i + 1, j)
-		self.helper(i, j + 1)
-
-		#make the count 
-
-
-	def uniquePaths(self,m,n):
-		"""
-		The main path function
-		"""
-
-		count = 0 
-
-		#constaint case 
-		if m == 1 and n == 1 :
+		if i == m -1 and j == n- 1:
 			return 1
 
+		if i >= m or j >= n :
+			return 0
 
-		self.helper(0,0,m,n)
+
+		#down_traveral 
+		down = self.helper(i,j + 1,m,n)
+		rigt = self.helper(i + 1,m,n)
 
 
-		return count
+		return down + right
+
+
+	def uniquePaths(self, m, n):
+		"""
+		The main function to calculate unique paths in a grid.
+		"""
+
+		# Constraint case: if the grid is 1x1, there's only 1 unique path
+		if m == 1 and n == 1:
+			return 1
+
+		# Call the recursive helper starting from the top-left corner (0, 0)
+		return self.helper(0, 0, m, n)
