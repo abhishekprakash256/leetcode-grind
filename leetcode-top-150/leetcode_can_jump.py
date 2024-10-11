c695172389f00a93b53160f8ea0ad187387dea30
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-10-11 15:14:07 -0400

    Code

---
+++ leetcode-top-150/leetcode_can_jump.py
@@ -0,0 +1,53 @@
+"""
+You are given an integer array nums. You are initially positioned at the array's first index, 
+and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+"""
+
+"""
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+[3,2,2,0,4]
+[3,2,1,0,4]
+
+
+
+"""
+
+
+"""
+optimal solution 
+
+#base case 
+if len(nums) == 1:
+    if nums[0] > 0 :
+        return True
+    else:
+        return False
+
+
+[0,0,0,0,0,0]
+
+
+
+
+
+
+
+
+
+
+
+
+"""
