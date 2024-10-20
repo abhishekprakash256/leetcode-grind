170e9eaec9be86dfe322d77b77d00a1bb457fc80
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-10-20 13:28:01 -0400

    code

--- leetcode-top-150/leetcode_candy.py
+++ leetcode-top-150/leetcode_candy.py
@@ -3,8 +3,8 @@ here are n children standing in a line. Each child is assigned a rating value gi
 
 You are giving candies to these children subjected to the following requirements:
 
-    Each child must have at least one candy.
-    Children with a higher rating get more candies than their neighbors.
+	Each child must have at least one candy.
+	Children with a higher rating get more candies than their neighbors.
 
 Return the minimum number of candies you need to have to distribute the candies to the children.
 
@@ -39,7 +39,49 @@ The third child gets 1 candy because it satisfies the above two conditions.
 
 
 
-
-
-
 """
+
+
+
+
+class Solution:
+	def candy(self, ratings) -> int:
+		"""
+		The function to find the number of the candy needed 
+		"""
+
+		#make the array with rating with 1 
+		arr = [1] * len(ratings)
+
+		
+		#make the array have more than value of the neighbour array 
+		for i in range(1,len(ratings)):
+
+			if ratings[i-1] < ratings[i]:
+				arr[i] = arr[i-1] + 1
+
+
+		#make the array to have more values 
+		for i in range(len(ratings) - 2, -1, -1):
+
+			if ratings[i] > ratings[i+1]:
+				arr[i] = max(arr[i], arr[i + 1] + 1)
+
+		return sum(arr)
+
+
+
+sol = Solution()
+
+print(sol.candy([1,0,2]))
+
+
+
+
+
+
+
+
+
+
+
