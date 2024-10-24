0c7d53ab567518f2bdbe70e8c459ab6e8f5fc419
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-10-24 02:01:19 -0400

    code

--- leetcode-top-150/leetcode_longest_common_prefix.py
+++ leetcode-top-150/leetcode_longest_common_prefix.py
@@ -44,12 +44,38 @@ return common_prefix
 
 
 class Solution(object):
-	def longestCommonPrefix(self, strs):
-		"""
-		:type strs: List[str]
-		:rtype: str
-		"""
-		
-		
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        # Base case: if the list is empty, return an empty string
+        if not strs:
+            return ""
+
+        # Initialize the common prefix to the first string
+        common_prefix = strs[0]
+
+        # Loop over the rest of the strings
+        for i in range(1, len(strs)):
+
+            # Find the minimum length between the current common prefix and the current string
+            length = min(len(common_prefix), len(strs[i]))
+
+            # Check for the common characters
+            for j in range(length):
+                if common_prefix[j] != strs[i][j]:
+                    # Truncate the common prefix at the point of difference
+                    common_prefix = common_prefix[:j]
+                    break
+
+            # After the inner loop, update the prefix to the length of the current string if needed
+            common_prefix = common_prefix[:length]
+
+            # If the common prefix is empty, there's no point in continuing
+            if not common_prefix:
+                return ""
+
+        return common_prefix
 
 		
