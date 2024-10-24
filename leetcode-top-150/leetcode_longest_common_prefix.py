82b480f28d3714e439d29e1af34d4e3587fb31f8
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-10-24 01:32:53 -0400

    code

--- leetcode-top-150/leetcode_longest_common_prefix.py
+++ leetcode-top-150/leetcode_longest_common_prefix.py
@@ -19,7 +19,37 @@ Explanation: There is no common prefix among the input strings.
 
 
 """
+#base case 
+for word in strs:
+	if len(word) == 0:
+		return ""
+
+
+common_prefix = strs[0]
+
+for i in range(1,len(strs) - 1 ):
+
+	length = min(len(common_prefix), len(strs[i]))
+
+	for j in range(0,length - 1 ):
+
+		if common_prefix[j] != strs[i][j]:
+			common_predix = common_preifx[j-1]
+
+return common_prefix
 
 
 
 """
+
+
+class Solution(object):
+	def longestCommonPrefix(self, strs):
+		"""
+		:type strs: List[str]
+		:rtype: str
+		"""
+		
+		
+
+		
