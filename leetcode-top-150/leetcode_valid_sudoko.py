97e8834c420d1475c531e9bb7874ae1d73f0ced5
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-10-29 20:37:16 -0400

    code

--- leetcode-top-150/leetcode_valid_sudoko.py
+++ leetcode-top-150/leetcode_valid_sudoko.py
@@ -119,66 +119,113 @@ def dfs(board[i][j], mapper):
 
 
 
+class Solution_brute:
+	# Helper function for 3x3 grid validation
+	def dfs(self, i, j, mapper):
+		"""
+		Recursive function to check a 3x3 grid.
+		"""
+		# Base case: stop when reaching the boundaries of a 3x3 grid
+		if i == 3 or j == 3:
+			return True
+		
+		# Check if the current cell is valid
+		if self.board[self.start_i + i][self.start_j + j] != ".":
+			num = self.board[self.start_i + i][self.start_j + j]
+			if num in mapper:
+				return False
+			mapper[num] = True
+		
+		# Recursively move to the next cell in the 3x3 grid
+		if j < 2:
+			return self.dfs(i, j + 1, mapper)
+		else:
+			return self.dfs(i + 1, 0, mapper)
+
+	def isValidSudoku(self, board: list) -> bool:
+		"""
+		Validates the Sudoku board.
+		passes leetcode
+		"""
+		self.board = board
+
+
+		# Row validation
+		for i in range(9):
+			row_mapper = {}
+			for j in range(9):
+				if self.board[i][j] != ".":
+					if self.board[i][j] in row_mapper:
+						return False
+					row_mapper[self.board[i][j]] = True
+
+		
+		# Column validation
+		for j in range(9):
+			col_mapper = {}
+			for i in range(9):
+				if self.board[i][j] != ".":
+					if self.board[i][j] in col_mapper:
+						return False
+					col_mapper[self.board[i][j]] = True
+
+		
+		# 3x3 subgrid validation using dfs
+		for i in range(0, 9, 3):
+			for j in range(0, 9, 3):
+				self.start_i, self.start_j = i, j  # Starting coordinates for the 3x3 grid
+				subgrid_mapper = {}
+				if not self.dfs(0, 0, subgrid_mapper):  # Start dfs within each 3x3 grid
+					return False
+
+		return True
+
+
+
+
+
+from typing import List
+
 class Solution:
-    # Helper function for 3x3 grid validation
-    def dfs(self, i, j, mapper):
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         """
-        Recursive function to check a 3x3 grid.
+        Function to check if a Sudoku board is valid.
         """
-        # Base case: stop when reaching the boundaries of a 3x3 grid
-        if i == 3 or j == 3:
-            return True
-        
-        # Check if the current cell is valid
-        if self.board[self.start_i + i][self.start_j + j] != ".":
-            num = self.board[self.start_i + i][self.start_j + j]
-            if num in mapper:
-                return False
-            mapper[num] = True
-        
-        # Recursively move to the next cell in the 3x3 grid
-        if j < 2:
-            return self.dfs(i, j + 1, mapper)
-        else:
-            return self.dfs(i + 1, 0, mapper)
-
-    def isValidSudoku(self, board: list) -> bool:
-        """
-        Validates the Sudoku board.
-        passes leetcode
-        """
-        self.board = board
-
 
         # Row validation
         for i in range(9):
             row_mapper = {}
             for j in range(9):
-                if self.board[i][j] != ".":
-                    if self.board[i][j] in row_mapper:
+                if board[i][j] != ".":
+                    if board[i][j] in row_mapper:
                         return False
-                    row_mapper[self.board[i][j]] = True
+                    row_mapper[board[i][j]] = True
 
-        
         # Column validation
-        for j in range(9):
+        for i in range(9):
             col_mapper = {}
-            for i in range(9):
-                if self.board[i][j] != ".":
-                    if self.board[i][j] in col_mapper:
+            for j in range(9):
+                if board[j][i] != ".":
+                    if board[j][i] in col_mapper:
                         return False
-                    col_mapper[self.board[i][j]] = True
+                    col_mapper[board[j][i]] = True
 
-        
-        # 3x3 subgrid validation using dfs
+        # 3x3 subgrid validation
         for i in range(0, 9, 3):
             for j in range(0, 9, 3):
-                self.start_i, self.start_j = i, j  # Starting coordinates for the 3x3 grid
                 subgrid_mapper = {}
-                if not self.dfs(0, 0, subgrid_mapper):  # Start dfs within each 3x3 grid
-                    return False
+                for x in range(i, i + 3):
+                    for y in range(j, j + 3):
+                        if board[x][y] != ".":
+                            if board[x][y] in subgrid_mapper:
+                                return False
+                            subgrid_mapper[board[x][y]] = True
 
+        # If all checks pass, the board is valid
         return True
+
+
+
 
 
 
