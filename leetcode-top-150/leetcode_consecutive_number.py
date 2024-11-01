cd2d71898189f153d59fdd72baec7e605de1de98
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-01 14:41:17 -0400

    Code

---
+++ leetcode-top-150/leetcode_consecutive_number.py
@@ -0,0 +1,41 @@
+"""
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+"""
+
+"""
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+"""
+
+
+"""
+
+max_long = 0
+
+nums.sort()
+
+[1,2,3,4,100,200]
+
+cur_length = 0 
+
+
+for i in range(1,len(nums)):
+
+    if nums[i] - nums[i-1] = 1:
+    
+        
+        
+        
+        
+
+
+
+"""
