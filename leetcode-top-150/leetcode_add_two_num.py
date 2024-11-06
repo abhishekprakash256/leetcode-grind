3ddf175632e93088249795590b029cd2615eb9b1
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-06 15:53:35 -0500

    code

---
+++ leetcode-top-150/leetcode_add_two_num.py
@@ -0,0 +1,52 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers. The digits are 
+stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+"""
+
+"""
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+"""
+
+node = ListNode()
+
+temp1 = l1
+temp2 = l2
+
+
+carry = 0 
+sum = 0
+
+while temp1 and temp2 :
+
+    #add
+
+
+
+
+
+
+
+"""
+
+
