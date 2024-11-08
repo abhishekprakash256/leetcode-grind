138ac1e2f83bb731f6d14c0fd3d2c88ec47eb5b5
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-08 10:50:09 -0500

    code

--- leetcode-top-150/exp.py
+++ leetcode-top-150/exp.py
@@ -10,3 +10,82 @@ val.add(4)
 print(val)
 
 print(len(" "))
+
+
+"""
+
+make the linked list
+"""
+
+class Node:
+    def __init__(self,val=None,next = None ):
+        self.val = val
+        self.next = next
+
+
+
+#make the node
+
+head = Node(1)
+node1 = Node(2)
+node2 = Node(3)
+node3 = Node(4)
+node4 = Node(5)
+
+#connect the linked list 
+head.next = node1
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+
+#print the list 
+
+class Solution():
+
+    def print_list(self,node):
+
+        temp = node
+
+        while temp:
+
+            print(temp.val)
+
+            temp = temp.next
+
+    
+    def rev_list(self,root):
+        """
+        The function to reverse the list 
+        """
+
+        #base case 
+        if not root :
+            return None
+
+
+        #ptr 
+        prev = None
+        curr = root
+
+        #start the loop 
+        while curr:
+
+            temp = prev 
+            prev = curr
+            curr = curr.next
+            prev.next = temp
+
+        
+
+
+
+
+
+
+
+
+sol = Solution()
+print(sol.print_list(head))
+sol.rev_list(head)
+print(sol.print_list(node4))
