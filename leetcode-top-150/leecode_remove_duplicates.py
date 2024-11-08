b228579e8b7a8a405f6e47e174033e359554edf3
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-08 16:18:19 -0500

    code

---
+++ leetcode-top-150/leecode_remove_duplicates.py
@@ -0,0 +1,69 @@
+"""
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, 
+leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+"""
+
+"""
+if not head:
+	return None 
+
+
+if not head.next :
+
+	return head
+
+
+curr = head 
+
+while curr :
+
+	if curr.val == curr.next.val :
+		
+		curr.next = curr.next.next
+	
+	else:
+	
+		curr = curr.next
+
+
+
+"""
+
+class Solution:
+	def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+		"""
+		The function to remove the duplicate node
+		"""
+
+		#base case 
+		if not head :
+			return None 
+
+		if head.next is None:
+			return head
+		
+
+		#make the ptr 
+		curr = head
+
+		#start the removal loop 
+
+		while curr:
+
+			if curr.val == curr.next.val :
+
+				if curr.next:
+
+					curr.next = curr.next.next
+
+				else:
+
+					curr.next = None
+
+			
+			else:
+				curr = curr.next
+			
+		
+		return head
