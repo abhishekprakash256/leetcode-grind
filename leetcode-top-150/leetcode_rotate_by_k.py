a9a44a63d8c207bebe10e97994baed6d6bd9bf3a
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-13 16:50:13 -0500

    code

--- leetcode-top-150/leetcode_rotate_by_k.py
+++ leetcode-top-150/leetcode_rotate_by_k.py
@@ -19,6 +19,23 @@ use two pointer
 one at head 
 other at length - k 
 
+
+#base case 
+if not head:
+    return None 
+
+if not head.next:
+    return node
+
+length = 1 
+
+temp = head 
+
+while temp:
+    length += 1
+    temp = temp.next
+
+
 if k < lenghth :
     second_ptr_pos = length - pos 
 
@@ -26,5 +43,95 @@ else:
 
     second_ptr_pos = length - (length//k)
 
+    
+r = head 
+
+for i in range(second_ptr):
+
+    r = r.next
+
+tail = head 
+
+#make to the last 
+while tail.next : 
+
+    tail = tail.next 
+
+#sawp the nodes 
+
+tail.next = head 
+secon_ptr_pos.next = None 
+
+
 
 """
+
+from typing import Optional , ListNode
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        """
+        The function to rotate the linked list by k times 
+        """
+
+        #base case 
+        if not head :
+            return None 
+
+        if not head.next : 
+            return head 
+        
+
+        #get the length 
+        length = 1 
+        temp = head 
+
+        while temp:
+
+            length += 1 
+            temp = temp.next
+
+        #get the second ptr pos 
+
+        if k < length:
+
+            r = length - k 
+        
+        else:
+
+            r = length - (length//k)
+
+        
+        #get the last node 
+        right = head 
+
+        while right.next:
+
+            right = right.next
+        
+
+        #put the seond ptr in pos 
+
+        left = head
+
+        for _ in range(length-r ):
+
+            left = left.next
+
+        
+        #swap the nodes
+
+        left.next = None 
+        right.next = head
+
+        return head
+        
+
+
+        
+
+
+
+
+            
