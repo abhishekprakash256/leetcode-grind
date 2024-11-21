2b77d3ac360b0fd72a497184974fac2d0e388936
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-21 05:33:12 -0500

    code

--- leetcode-top-150/leetcode_tree_populate_next.py
+++ leetcode-top-150/leetcode_tree_populate_next.py
@@ -2,10 +2,10 @@
 Given a binary tree
 
 struct Node {
-  int val;
-  Node *left;
-  Node *right;
-  Node *next;
+	int val;
+	Node *left;
+	Node *right;
+	Node *next;
 }
 
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
@@ -41,56 +41,96 @@ queue is used in bfs
 
 """
 
+
+
+class Node:
+
+	def __init__(self,val = None, left = None, right = None , next = None):
+
+		self.val = val
+		self.left = left
+		self.right = right
+		self.next = next
+
+
+
+#make node 
+
+root = Node(1)
+node1 = Node(2)
+node2 = Node(3)
+node3 = Node(4)
+node4 = Node(5)
+node5 = Node(7)
+
+
+#connect the trees
+
+root.left = node1
+root.right = node2
+node1.left = node3
+node1.right = node4
+node2.right = node5
+
 class Solution(object):
-    def connect(self, root):
-        """
-        :type root: Node
-        :rtype: Node
-        """
+		def connect(self, root):
+				"""
+				:type root: Node
+				:rtype: Node
+				passes leetcode
+				"""
 
-        #base case 
-        if not root :
+				#base case 
+				if not root :
 
-          return None
-
-
-
-        #make the queue
-        queue = [root]
-
-        #make the res lst 
-        res_lst = []
-
-
-        while queue :
-
-          temp_lst = []
-
-          for _ in range(len(queue)):
-
-            temp_node = queue.pop(0)
-
-            if temp_node:
-
-              temp_lst.append(temp_node)
-
-              queue.append(temp_node.left)
-
-              queue.append(temp_node.right)
-
-          res_lst.append(temp_lst)
-
-
-        #connect the nodes 
-        for nodes in res_lst : 
-
-            for i in range(len(nodes)-1) :
-
-                nodes[i].next = nodes[i+1]
-
-            nodes[len(nodes)-1].next = None
+					return None
 
 
 
+				#make the queue
+				queue = [root]
+
+				#make the res lst 
+				res_lst = []
 
 
+				while queue :
+
+					temp_lst = []
+
+					for _ in range(len(queue)):
+
+						temp_node = queue.pop(0)
+
+						if temp_node:
+
+							temp_lst.append(temp_node)
+
+							queue.append(temp_node.left)
+
+							queue.append(temp_node.right)
+
+					res_lst.append(temp_lst)
+
+
+				#connect the nodes 
+				for nodes in res_lst : 
+
+					print(nodes)
+
+					for i in range(len(nodes)-1) :
+
+							nodes[i].next = nodes[i+1]
+
+					if nodes:
+
+						nodes[len(nodes)-1].next = None
+
+
+				return root
+
+
+
+
+sol = Solution()
+print(sol.connect(root))
