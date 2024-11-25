60cf4cbf62700d2bd83667f0ca8f3a363db5cc33
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-11-25 02:14:59 -0500

    code

--- leetcode-top-150/leetcode_sum_root_leaf.py
+++ leetcode-top-150/leetcode_sum_root_leaf.py
@@ -57,3 +57,79 @@ type cast to all values to int
 
 
 
+#make node 
+
+class Node:
+
+    def __init__(self,val = None, left = None, right = None , next = None):
+
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+root = Node(1)
+node1 = Node(2)
+node2 = Node(3)
+node3 = Node(4)
+node4 = Node(5)
+node5 = Node(7)
+
+
+#connect the trees
+
+root.left = node1
+root.right = node2  
+
+
+
+class Solution:
+    def __init__(self):
+        self.res_lst = []  # To store all root-to-leaf paths
+
+    def helper(self, node, temp_str):
+        """
+        Helper function to recursively traverse the tree
+        and collect root-to-leaf paths.
+        """
+        if not node:
+            return
+
+        # Append the current node value to the path string
+        temp_str += str(node.val)
+
+        # If the node is a leaf, append the path to res_lst
+        if not node.left and not node.right:
+            self.res_lst.append(temp_str)
+        else:
+            # Recur for left and right subtrees
+            self.helper(node.left, temp_str)
+            self.helper(node.right, temp_str)
+
+    def sumNumbers(self, root):
+        """
+        Function to collect all root-to-leaf paths as strings.
+        """
+        if not root:
+            return []  # Return an empty list if the tree is empty
+
+        # Clear previous results and start the DFS traversal
+        self.res_lst = []
+        self.helper(root, "")
+
+        for i in range(len(self.res_lst)):
+
+            self.res_lst[i] = int(self.res_lst[i])
+        
+
+
+
+        return sum(self.res_lst)
+
+
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.dfs_carry(root))
