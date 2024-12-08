2990663cf6d38a1deda359e548ce63b6d129169a
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-08 13:09:02 -0500

    code

--- leetcode-top-150/leetcode_number_island.py
+++ leetcode-top-150/leetcode_number_island.py
@@ -35,6 +35,56 @@ make a dfs solution using the
 
 
 
-
-
 """
+
+
+
+class Solution(object):
+
+	def dfs_graph(self,i,j):
+		"""
+		The function to get the dfs of the graph
+		"""
+
+		#base case 
+		if self.grid[i][j] == "0" or i < 0 or j > len(self.grid[0]) - 1  or i > len(self.grid)-1 or j < 0 :
+
+			return None
+
+
+		#make the grid 0 
+		self.grid[i][j] = "0"
+
+		#make the dfs graph call 
+		self.dfs_graph(i-1,j)
+		self.dfs_graph(i+1,j)
+		self.dfs_graph(i,j-1)
+		self.dfs_graph(i,j-1)
+
+
+
+
+
+	def numIslands(self, grid):
+		"""
+		:type grid: List[List[str]]
+		:rtype: int
+		"""
+
+		self.grid = grid
+		count = 0 
+
+
+		#make the iterative search
+		for i in range(len(self.grid)-1) :
+
+			for j in range(len(self.grid[0])-1) :
+
+				if self.grid[i][j] == "1":
+
+					count +=1
+				
+				self.dfs_graph(i,j)
+
+		return count
+
