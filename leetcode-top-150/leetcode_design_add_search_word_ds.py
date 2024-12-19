32289c1df34a8748f1a5d399c0c6665989274089
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-19 16:20:46 -0500

    code

--- leetcode-top-150/leetcode_design_add_search_word_ds.py
+++ leetcode-top-150/leetcode_design_add_search_word_ds.py
@@ -10,10 +10,13 @@ bool search(word) Returns true if there is any string in the data structure that
 """
 
 """
+
 Input
 ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
 [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+
 Output
+
 [null,null,null,null,false,true,true,true]
 
 Explanation
@@ -26,6 +29,104 @@ wordDictionary.search("bad"); // return True
 wordDictionary.search(".ad"); // return True
 wordDictionary.search("b.."); // return True
 
+"""
+
+"""
+approach --- 
+
+waht about a trie ? 
+
+what abot the ".." matching ?  
+
+
+class TrieNode:
+
+	def __init__(self):
+		self.children = {}
+		self.end = False
+
+
+class WordDictionary:
+
+	def __init__(self):
+		self.root = TrieNode() 
+
+
+
+	def addWord(self,word) :
+
+		node = self.root 
+
+		for char in word :
+
+			if char not in self.children :
+
+				self.children[char] = TrieNode()
+
+			node = node.children[char]
+
+		node.end = True
+
+
+	def search(self,word):
+
+		node = self.root
+
+		for char in word :
+
+			if char != "." :
+
+				if char not in self.children:
+					return False
+
+			node = node.children[char]
+
+		return True
+		
+
+
 
 """
 
+class TrieNode:
+
+	def __init__(self):
+		self.children = {}
+		self.end = False
+
+
+class WordDictionary:
+
+	def __init__(self):
+		self.root = TrieNode() 
+
+
+	def addWord(self,word) :
+
+		node = self.root 
+
+		for char in word :
+
+			if char not in node.children :
+
+				node.children[char] = TrieNode()
+
+			node = node.children[char]
+
+		node.end = True
+
+
+	def search(self,word):
+
+		node = self.root
+
+		for char in word :
+
+			if char != "." :
+
+				if char not in node.children:
+					return False
+
+			node = node.children[char]
+
+		return True
