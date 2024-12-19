9d6774bf17c35d583ef661fc99f48f2dfd552d41
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-19 00:16:30 -0500

    code

--- leetcode-top-150/leetcode_minimum_genetic_mutation.py
+++ leetcode-top-150/leetcode_minimum_genetic_mutation.py
@@ -18,9 +18,60 @@ Note that the starting point is assumed to be valid, so it might not be included
 
 
 """
+approach -- 
+
 
 
 
 
 
 """
+
+
+class Solution:
+	def minMutation(self,start,end,bank):
+		"""
+		The function to find the min value
+		"""
+
+		#base case 
+		if start == end :
+			return 0
+
+
+		#make the queue 
+		queue = [(start,0)]
+
+		#make the seen set
+		seen = set()
+
+		#star the loop
+		while queue :
+
+			#take the node out 
+			node ,step = queue.pop(0)
+
+			#make the matching case
+			if node == end :
+				return step
+
+			#explore the neighbor 
+
+			for char in "ACGT" :
+
+				for i in range(len(node)) :
+
+					neighbor = node[:i] + char + node[i+1:]
+
+					if neighbor not in seen and neighbor in bank:
+
+						queue.append((neighbor,step + 1 ))
+						seen.add(neighbor)
+
+		return -1
+
+
+
+
+
+
