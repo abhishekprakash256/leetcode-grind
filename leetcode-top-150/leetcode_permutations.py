81e630395d1a01120337587066aa5c76cbe871e7
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-20 10:55:02 -0500

    code

---
+++ leetcode-top-150/leetcode_permutations.py
@@ -0,0 +1,15 @@
+"""
+
+Given an array nums of distinct integers, return all the possible 
+permutations. You can return the answer in any order
+"""
+
+
+"""
+
+
+"""
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
