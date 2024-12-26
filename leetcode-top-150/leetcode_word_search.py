715750c8e1482274df7d00805c1c7e2a41927ce0
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-25 22:07:22 -0500

    code

---
+++ leetcode-top-150/leetcode_word_search.py
@@ -0,0 +1,104 @@
+"""
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, 
+where adjacent cells are horizontally or vertically neighboring. The same letter cell may not 
+be used more than once.
+"""
+
+"""
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+
+
+
+"""
+
+
+class Solution():
+
+	def helper_dfs(self,i,j,res_lst):
+		"""
+		The function to make the dfs of the word grid
+		"""
+			
+		#base case 
+		if i < 0 or j < 0 or i > len(self.board)-1 or j > len(self.board[0])-1 or self.board[i][j] == "#" :
+
+			return 
+
+
+		#check for the word
+		if res_lst == self.word :
+
+			return True
+
+		#mark the board
+		self.board[i][j] = "#" 
+
+		print(res_lst)
+
+
+		#start the dfs search
+		self.helper_dfs(i-1,j,res_lst + [self.board[i][j]])
+		self.helper_dfs(i+1,j,res_lst + [self.board[i][j]])
+		self.helper_dfs(i,j-1,res_lst + [self.board[i][j]])
+		self.helper_dfs(i,j+1,res_lst + [self.board[i][j]])
+
+
+
+
+	def exist(self,board,word):
+		"""
+		The function to search the board
+		"""
+
+		self.board = board
+		self.word = list(word)
+
+		#constraints 
+		if len(self.board) == 1 and len(self.board[0]) == 1 and self.board[0] == self.word:
+
+			return True
+
+
+		#start the dfs recursion 
+		for i in range(len(self.board)) :
+
+			for j in range(len(self.board[0])) :
+
+				if self.board[i][j] != "#":
+
+					if self.helper_dfs(i,j,[]) :
+
+						return True
+
+
+		return False
+
+
+
+
+
+if __name__ == '__main__':
+	sol = Solution()
+	print(sol.exist(board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"))
+
+
