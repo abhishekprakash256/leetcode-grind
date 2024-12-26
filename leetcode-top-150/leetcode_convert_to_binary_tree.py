1dda83c238f05842fd3352ef426b24aee97dd506
abhishekprakash256 <abhishekprakash47@gmail.com>
2024-12-26 14:45:33 -0500

    code

--- leetcode-top-150/leetcode_convert_to_binary_tree.py
+++ leetcode-top-150/leetcode_convert_to_binary_tree.py
@@ -31,3 +31,75 @@ nums is sorted in a strictly increasing order.
 #         self.right = right
 
 """
+
+"""
+
+approach -- 
+
+sorted helps 
+
+find the middle and make the head or root node 
+
+len(nums) // 2 = mid 
+
+root = TreeNode(nums[mid])
+
+root.left = self.helper()
+
+
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+
+	def helper(self,i,j):
+		"""
+		The helper function for recusive node making
+		"""
+
+		#base case 
+		if i == j :
+			return 
+
+		#make the node 
+		mid = (i+j) // 2 
+		node = TreeNode(self.nums[mid])
+
+		#get the head node 
+		if i == 0 and j == len(self.nums) - 1 :
+			self.headnode = node
+
+		#make the recursive node calls 
+		node.left = self.helper(i,mid)
+		node.right = self.helper(mid+1,j)
+
+
+
+	def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+		"""
+		The function to make the tree
+		"""
+		self.nums = nums
+
+		#constraints 
+		if len(nums) == 1 :
+			node = TreeNode(nums[0])
+			return node
+
+		#make the recursion call
+		self.helper(0,len(self.nums)-1)
+
+		return self.headnode
+
+
+
+
+
