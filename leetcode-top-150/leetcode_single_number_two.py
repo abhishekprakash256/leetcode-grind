277c141c87fe9eaa9c44ee978aa4655338bb4ae0
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-04 01:37:53 -0500

    code

--- leetcode-top-150/leetcode_single_number_two.py
+++ leetcode-top-150/leetcode_single_number_two.py
@@ -4,3 +4,21 @@ Given an integer array nums where every element appears three times except for o
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 """
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+    	"""
+    	passes leetcode
+
+    	"""
+        ones, twos = 0, 0
+        
+        for num in nums:
+            # Update 'ones' with the current number, excluding bits in 'twos'
+            ones = (ones ^ num) & ~twos
+            
+            # Update 'twos' with the current number, excluding bits in 'ones'
+            twos = (twos ^ num) & ~ones
+            
+        return ones
