2d49ba09e9146293ca3c6f5d9d2ebb63597054a7
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-08 15:43:47 -0500

    code

--- leetcode-top-150/leetcode_longest_increasing_sequence.py
+++ leetcode-top-150/leetcode_longest_increasing_sequence.py
@@ -12,7 +12,7 @@ Example 1:
 Input: nums = [10,9,2,5,3,7,101,18]
 Output: 4
 Explanation: The longest increasing subsequence is [2,3,7,101],
-therefore the length is 4, 2,5,3,7,101 -> 2,3,7,101
+therefore the length is 4, 2,5,3,7,101 -> 2,5,3,7,101
 
 Example 2:
 
@@ -42,3 +42,31 @@ if len(nums) == 1:
 
 
 """
+
+
+class Solution():
+
+	def lengthOfLIS(self,nums) :
+		"""
+		The function to find the longest sub sequence
+		"""
+
+		#constarints 
+		if len(nums) == 1 :
+			return 1 
+
+
+		#make the dp array 
+		dp = [1] * len(nums)
+
+		#star the iter
+		for i in range(1,len(nums)) :
+
+			for j in range(i) :
+
+				if nums[i] > nums[j] :
+
+					dp[i] = max(dp[i], dp[j]+ 1)
+
+
+		return max(dp)
