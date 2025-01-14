6ccb11b3a4c7dfaa163ace0bd5574a89c43be72b
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-13 20:52:37 -0500

    code

--- leetcode-random/leetcode_integer_roman.py
+++ leetcode-random/leetcode_integer_roman.py
@@ -63,7 +63,7 @@ Constraints:
 
 
 """
-
+approach -- 
 
 I	1
 V	5
@@ -73,8 +73,68 @@ C	100
 D	500
 M	1000
 
+{ 1:I, 2 : II , 3 : III , 4 : IV , 5: V , 6: VI , 7:VII ,8: VIII, 9: IX, 10 : X, 40: XL , 50 :L , 90 : XC , 100 : C, 200 : CC, 300 : CCC , 600 : DC , 700 : DCC , 800 , 2000 : MM , 3000 : MMM ,450 : LD,500:D, 900:CD ,950 :LD,1000 : M }
 
-{1:I,4 :IV , 5 :V , 9: IX, 10 : X, 40: XL ,  50 :L , 90 : XC , 100 : C }
 
+103 = 100 + 3 
+
+99 : 
+90 + 9 
+
+103 = 100 + 3 
+
+3999 = 3000 + 900 + 90 + 9 
+
+1000 + 1000 + 1000 + 900 + 90 + 9 
+
+390 = 300 + 90 
+
+
+390 // 100 = 3 
+
+390 % 100 = 90 
 
 """
+
+
+class Solution():
+
+	def intToRoman(self,num):
+		"""
+		The function to make the int to Roman 
+		pass leet code
+		"""
+		mapper = {0 : "" ,1: "I", 2 : "II" , 3 : "III" , 4 : "IV" , 5: "V" , 6: "VI" , 7: "VII" ,8: "VIII", 9: "IX", 10 : "X", 20 : "XX" , 30 : "XXX", 40: "XL" , 50 :"L" , 60 : "LX" , 70: "LXX", 80 : "LXXX", 90 : "XC" , 100 : "C", 200 : "CC", 300 : "CCC" , 400 : "CD" , 600 : "DC" , 700 : "DCC" , 800 :"DCCC" , 2000 : "MM" , 3000 : "MMM" ,450 : "LD",500:"D", 900:"CM" , 950 :"LD",1000 : "M" }
+
+
+		#initilaize the empty string
+		roman_number = ""
+
+		#divide and get remainder 
+		thousand = num // 1000 
+		rem = num % 1000 
+
+		roman_number += mapper[thousand*1000]
+
+		hundred = rem // 100
+		rem = rem % 100
+
+		roman_number += mapper[hundred*100]
+
+		tenth = rem // 10 
+		rem = rem % 10
+
+		roman_number += mapper[tenth*10]
+
+		ones = rem
+
+		roman_number += mapper[ones]
+
+		return roman_number
+
+
+
+sol = Solution()
+
+print(sol.intToRoman(3749))
+
