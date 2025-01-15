999ea522fe7e32165c8ea8d8f58bf31c62c0d2f6
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-14 21:47:37 -0500

    code

--- leetcode-random/leetcode_longest_valid_paranthesis.py
+++ leetcode-random/leetcode_longest_valid_paranthesis.py
@@ -93,14 +93,15 @@ class Solution():
 
 				l ,r = 0,0
 
-		l , r = 0,0
+		
+		l,r = 0,0
 		#reverse the logic to check 
 
 		for para in reversed(s) :
 
 			if para == ")" :
 
-				l += 1 
+				l += 1
 
 			if para == "(" :
 
