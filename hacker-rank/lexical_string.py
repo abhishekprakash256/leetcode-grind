ad76cfba17301e166719c2033071424a401e3e75
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-24 15:20:41 -0500

    code

---
+++ hacker-rank/lexical_string.py
@@ -0,0 +1,59 @@
+"""
+when dealing with strings. A string is greater than another string if it comes later in a 
+lexicographically sorted list.
+
+Given a word, create a new word by swapping some or all of its characters.
+ This new word must meet two criteria:
+
+It must be greater than the original word
+It must be the smallest word that meets the first condition
+Example
+
+The next largest word is .
+
+Complete the function biggerIsGreater below to create and return the new string meeting the criteria. If it is not possible, return no answer.
+
+Function Description
+
+Complete the biggerIsGreater function in the editor below.
+
+biggerIsGreater has the following parameter(s):
+
+string w: a word
+Returns
+- string: the smallest lexicographically higher string possible or no answer
+
+Input Format
+
+The first line of input contains , the number of test cases.
+Each of the next  lines contains .
+
+Constraints
+
+ will contain only letters in the range ascii[a..z].
+Sample Input 0
+
+5
+ab
+bb
+hefg
+dhck
+dkhc
+Sample Output 0
+
+ba
+no answer
+hegf
+dhkc
+hcdk
+
+"""
+
+"""
+approach -- 
+
+
+
+
+"""
+
