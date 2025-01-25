f073124b6eb6a764b41657842e80b7bee1d30fb2
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-25 14:28:50 -0500

    code

--- mixed-practice/leetcode_number_ilslands.py
+++ mixed-practice/leetcode_number_ilslands.py
@@ -43,63 +43,176 @@ grid[i][j] is '0' or '1'.
 """
 
 
+class Solution_dfs():
+    """
+    passes leetcode
+    """
+
+    def __init__(self):
+
+        self.count = 0
+
+    def helper_dfs(self,i,j):
+        """
+        The function to do the dfs traversal for the grid
+        """
+
+        #base case 
+        if i < 0 or j < 0 or i > self.row - 1 or j > self.col - 1  or self.grid[i][j] == "0":
+
+            return
+
+        #make the grid "0"
+        self.grid[i][j] = "0"
+
+        #make the recursive call
+        up = self.helper_dfs(i-1,j)
+        down = self.helper_dfs(i+1,j)
+        left = self.helper_dfs(i,j-1)
+        right = self.helper_dfs(i,j+1)
+
+
+
+    def numIslands(self,grid):
+        """
+        The function to find the number of the islands in the grid 
+        """
+
+        #make the vars
+        self.grid = grid
+
+        self.row = len(self.grid)
+        self.col = len(self.grid[0])
+
+        #iterate the rows and the cols 
+        for i in range(self.row) :
+
+            for j in range(self.col) :
+
+                if self.grid[i][j] == "1" :
+
+                    self.count += 1 
+
+                self.helper_dfs(i,j)
+
+        return self.count
+
+
+
+
+
+
+from collections import deque
+
+
+
 class Solution():
-	"""
-	passes leetcode
-	"""
+    """
+    The solution using bfs 
+    """
 
-	def __init__(self):
+    def __init__(self):
 
-		self.count = 0
+        self.count = 0
 
-	def helper_dfs(self,i,j):
-		"""
-		The function to do the dfs traversal for the grid
-		"""
+    def helper_bfs(self,i,j) :
+        """
+        The helper function to do bfs
+        """
+        
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)] #the possible 
 
-		#base case 
-		if i < 0 or j < 0 or i > self.row - 1 or j > self.col - 1  or self.grid[i][j] == "0":
+        queue = deque([(i,j)])
 
-			return
+        #make the mark
+        self.grid[i][j] = "0"
 
-		#make the grid "0"
-		self.grid[i][j] = "0"
+        while queue :
 
-		#make the recursive call
-		up = self.helper_dfs(i-1,j)
-		down = self.helper_dfs(i+1,j)
-		left = self.helper_dfs(i,j-1)
-		right = self.helper_dfs(i,j+1)
+            #pop the queue
+            x, y = queue.popleft()
 
+            #traverse all the dirs
+            for dx , dy in dirs :
 
+                nx , ny = x + dx , y + dy
 
-	def numIslands(self,grid):
-		"""
-		The function to find the number of the islands in the grid 
-		"""
+                #make the bound conditio
+                if 0 <= nx <= self.row - 1 and 0 <= ny <= self.col - 1 and self.grid[nx][ny] == "1" :
 
-		#make the vars
-		self.grid = grid
+                    #mark as visited
+                    self.grid[nx][ny] = "0"
 
-		self.row = len(self.grid)
-		self.col = len(self.grid[0])
-
-		#iterate the rows and the cols 
-		for i in range(self.row) :
-
-			for j in range(self.col) :
-
-				if self.grid[i][j] == "1" :
-
-					self.count += 1 
-
-				self.helper_dfs(i,j)
-
-		return self.count
+                    #append to queue 
+                    queue.append((nx,ny))
 
 
 
+    def numIslands(self,grid) :
+        """
+        The function to find the number of island in grids
+        """
+
+        self.grid = grid
+        self.row = len(self.grid)
+        self.col = len(self.grid[0])
 
 
-		
+        #constarint case
+        if self.row == 1 and self.col == 1 :
+
+            if self.grid[0][0] == "1" :
+
+                return 1
+
+            else :
+
+                return 0
+
+        #make the taveral 
+        for i in range(self.row) :
+
+            for j in range(self.col) :
+
+                #find the grid value that is 1
+                if self.grid[i][j] == "1" :
+
+                    self.count += 1 
+
+                    self.helper_bfs(i,j)
+
+
+        return self.count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
