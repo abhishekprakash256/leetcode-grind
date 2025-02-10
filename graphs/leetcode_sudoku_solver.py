08699d60bec5e2f0b4e10faf1b2549019af68524
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-10 17:14:07 -0500

    code

---
+++ graphs/leetcode_sudoku_solver.py
@@ -0,0 +1,56 @@
+"""
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+"""
+
+"""
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution.
+
+"""
+
+"""
+approach -- 
+
+with each row and col we maintain a set ? 
+
+how we add elemnets ? 
+
+what about 3X3 grid ? 
+
+how we do that ? 
+
+make a radnom filling value using radom ? 
+genetatr the random till not left 
+
+fill by 3X3 grid? and use dfs to search if that exists in that grid ? 
+
+if not exists keep it fill other ? 
+
+we can add the elemnet as well from 3X3 grid while scaing so chekc that as well ? 
+
+fist scan get the elements ? 
+
+second start filling ? 
+
+
+"""
+
