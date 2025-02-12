87f8b0c97579177e2ed089aa4c13b42ecd0d3304
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-11 23:21:02 -0500

    code

---
+++ revisions/hash-map/leetcode_first_missing_positive.py
@@ -0,0 +1,61 @@
+"""
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+"""
+
+"""
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+
+"""
+
+"""
+
+approach -- 
+
+O(n) and O(1) space 
+
+brute force 
+
+max_num = max(nums)
+
+for i in range(1, max_num + 1 ):
+
+    if i not in nums :
+        
+        return i
+
+"""
+
+class Solution_wrong():
+
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        """
+        The function to find the first posirtive integer 
+        """
+
+        max_num = max(nums)
+
+        for i in range(1,max_num + 1 ) :
+
+            if i not in nums :
+
+                return i
+
+        return i + 1 
