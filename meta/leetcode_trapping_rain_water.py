0ee41be68df2bc324a2b7f2bb6a4441ce00f29e8
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-17 13:52:05 -0500

    code

---
+++ meta/leetcode_trapping_rain_water.py
@@ -0,0 +1,22 @@
+"""
+Given n non-negative integers representing an elevation map where the width of 
+each bar is 1,
+compute how much water it can trap after raining.
+"""
+
+"""
+approach -- 
+
+use a two pointer from left and right 
+
+constarint case if n == 1 ;
+
+make two pointers l , r = 0 ,len(nums) - 1
+
+left_max = 0 
+right_max = 0 
+
+
+
+
+"""
