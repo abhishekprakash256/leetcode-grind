1944aa3f363b32403063089cc80f612871f4a5b8
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-18 14:02:16 -0500

    code

--- meta/leetcode_simplify_paths.py
+++ meta/leetcode_simplify_paths.py
@@ -80,9 +80,42 @@ path is a valid absolute Unix path.
 """
 
 """
-
 approch using stack -- 
 
 
 
 """
+
+
+class Solution:
+    """
+    passes leetcode
+    """
+
+    def simplifyPath(self, path: str) -> str:
+
+        # Initialize a stack
+        stack = []
+
+        # Split the input string on "/" as the delimiter
+        # and process each portion one by one
+        for portion in path.split("/"):
+
+            if portion == ".." :
+
+                if stack :
+
+                    stack.pop()
+
+            elif portion == "." or not portion:
+                # A no-op for a "." or an empty string
+                continue
+
+            else:
+                # Finally, a legitimate directory name, so we add it
+                # to our stack
+                stack.append(portion)
+
+        # Stich together all the directory names together
+        final_str = "/" + "/".join(stack)
+        return final_str
