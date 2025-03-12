fe1d5f40335f113874350643dc31208a5d764bb1
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-12 16:46:51 -0400

    bfs iter

--- meta/leetcode_binary_tree_iterator.py
+++ meta/leetcode_binary_tree_iterator.py
@@ -33,7 +33,80 @@ approach --
 
 using a dfs traversal , left , root , right
 
+use a dfs fucnton with res to craete a list array and append the elements 
 
+the interate the list 
 
 
 """
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+
+        self.root = root
+        self.result = []
+        self.i = 0
+        self._dfs_iter(self.root)
+
+
+    def _dfs_iter(self,node):
+        """
+        The function to make the dfs iter for the tree
+        """
+
+        #make the iter 
+
+        #base case 
+        if node :
+            
+            if node.left :
+                
+                self._dfs_iter(node.left)
+
+            self.result.append(node.val)
+
+
+            if node.right :
+
+                self._dfs_iter(node.right)
+
+
+        
+
+    def next(self) -> int:
+        """
+        The function to get the next value 
+        """
+
+        temp = self.i 
+
+        self.i += 1 
+
+        return self.result[temp]
+
+
+
+
+    def hasNext(self) -> bool:
+        """
+        The function to check the next value exist or not 
+        """
+
+        if self.i <= len(self.result) - 1 :
+
+            return self.result[self.i]
+
+        return False
