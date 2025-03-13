df9372cb0ee224be35433f89a8c2011158773d46
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-12 20:11:56 -0400

    rearrange string

--- amazon/leetcode_reorganize_strings.py
+++ amazon/leetcode_reorganize_strings.py
@@ -103,18 +103,29 @@ class Solution:
         heapify(pq)
 
         while pq:
+
             count_first, char_first = heappop(pq)
+
             if not ans or char_first != ans[-1]:
+
                 ans.append(char_first)
                 if count_first + 1 != 0: 
                     heappush(pq, (count_first + 1, char_first))
+
             else:
+                
                 if not pq: return ''
+
                 count_second, char_second = heappop(pq)
+
                 ans.append(char_second)
+
                 if count_second + 1 != 0:
+
                     heappush(pq, (count_second + 1, char_second))
+
                 heappush(pq, (count_first, char_first))
+
 
         return ''.join(ans)
 
