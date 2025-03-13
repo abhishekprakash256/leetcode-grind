8b79c8130b45d96c28fa489974427e853d26359c
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-13 01:39:59 -0400

    subarray sum wrong solution

--- amazon/leetcode_subarray_sum.py
+++ amazon/leetcode_subarray_sum.py
@@ -17,11 +17,69 @@ Output: 2
 
 
 
-"""
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+
 
 """
 
+"""
+sliding window ? 
 
+when sum is less add when greter slide the left pointer 
 
+and increase the counter for subarray 
 
 """
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        #constraints 
+
+        if len(nums) == 1 :
+
+            if nums[0] == k :
+
+                return 1 
+
+            else :
+
+                return -1
+
+        #ptrs 
+        l = 0 
+
+        count = 0 #array counter 
+
+        run_sum = nums[0] #running sum
+
+        #start the loop 
+        for i in range(1,len(nums)) :
+
+            if run_sum == k :
+
+                count += 1 
+
+            run_sum += nums[i]
+
+            while run_sum > k :
+
+                run_sum -= nums[l]
+
+                l += 1
+
+        return count
+
+
+
+
+        
