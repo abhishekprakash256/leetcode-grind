b1f84d687de5b7a4a3c8505c68a05157d7df9e33
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-17 17:18:06 -0400

    basic calculator 2

---
+++ amazon/leetcode_basic_calculator_2.py
@@ -0,0 +1,116 @@
+"""
+Given a string s which represents an expression, evaluate this expression and return its 
+value. 
+
+The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid.
+All intermediate results will be in the range of [-231, 231 - 1].
+
+Note: You are not allowed to use any built-in function which evaluates strings as 
+mathematical expressions, such as eval().
+"""
+
+
+"""
+
+Example 1:
+
+Input: s = "3+2*2"
+Output: 7
+Example 2:
+
+Input: s = " 3/2 "
+Output: 1
+Example 3:
+
+Input: s = " 3+5 / 2 "
+Output: 5
+"""
+
+
+"""
+approach using a stack ? 
+
+pop when see the sign ? 
+
+BODMAS
+
+how to keep the prioriy use the if conditons ? 
+
+DMAS
+
+start reding from right to left 
+
+append in array as see the number and pop 
+
+store the result and calculate 
+
+"""
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        """
+        The function to do the calculation
+        """
+
+        #constaint case 
+        if len(s) == 1 :
+
+            return int(s)
+
+
+        #make the stack 
+        stack = []
+
+        #ptrs
+        r = len(s) - 1 
+
+        #opertor set
+        operator = ("*","/","-","+") 
+
+
+        while r >= 0 :
+
+            if s[r] not in operator :
+
+                stack.append(int(s[r]))
+
+            elif s[r] == "/" :
+
+                val = stack.pop()
+
+                res = int(s[r-1]) / val
+
+                stack.append(int(res))
+
+            elif s[r] == "*" :
+
+                val = stack.pop()
+
+                res = int(s[r-1]) * val
+                stack.append(int(res))
+
+            elif s[r] == "+" :
+
+                val = stack.pop()
+
+                res = int(s[r-1]) + val 
+
+                stack.append(int(res))    
+
+
+            elif s[r] == "-" :
+
+                val = stack.pop()
+
+                res = int(s[r-1]) - val
+
+            
+                stack.append(int(res))
+
+            r -= 1
+
+        return stack[0] 
+
