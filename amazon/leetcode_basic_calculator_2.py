6afd2c13136af0c8131ee80359d552c5715c44a9
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-19 17:21:57 -0400

    random pick

--- amazon/leetcode_basic_calculator_2.py
+++ amazon/leetcode_basic_calculator_2.py
@@ -49,7 +49,7 @@ store the result and calculate
 """
 
 
-class Solution:
+class Solution_wrong():
     """
     wrong solution 
     """
@@ -118,6 +118,112 @@ class Solution:
             r -= 1
 
         return res
+
+
+
+
+
+
+
+"""
+
+Example 1:
+
+Input: s = "3+2*2"
+Output: 7
+Example 2:
+
+Input: s = " 3/2 "
+Output: 1
+Example 3:
+
+Input: s = " 3+5 / 2 "
+Output
+
+approach -- 
+
+use stack -- 
+
+if see a digit put in stack 
+
+if not a digit 
+
+3 + 2 * 2 
+
+append the first elemnt in the stack 
+
+res = stack.pop()
+
+[3]
+
+res = element 
+
+
+
+
+
+
+
+"""
+
+class Solution_wrong():
+    """
+    Doen't pass leet code
+    """
+
+    def evaluate_expression(s: str) -> int:
+        # Remove spaces from the string
+        s = s.replace(" ", "")
+        
+        # Initialize variables
+        stack = []
+        current_num = 0
+        sign = 1  # 1 for positive, -1 for negative
+        operation = '+'
+        
+        for i, char in enumerate(s):
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            
+            # If the current character is an operator or we've reached the end of the string
+            if char in "+-*/" or i == len(s) - 1:
+                if operation == '+':
+                    stack.append(sign * current_num)
+                elif operation == '-':
+                    stack.append(-sign * current_num)
+                elif operation == '*':
+                    stack[-1] = stack[-1] * current_num
+                elif operation == '/':
+                    # Handle division with truncation towards zero
+                    if stack[-1] < 0:
+                        stack[-1] = -(-stack[-1] // current_num)
+                    else:
+                        stack[-1] = stack[-1] // current_num
+                
+                # Reset for the next number
+                current_num = 0
+                sign = 1  # Default is positive sign
+                if char == '-':
+                    sign = -1
+                operation = char
+        
+        # Return the sum of the stack (which contains the final result)
+        return sum(stack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
