02b53038af6bc2582dedfb91046e7f010f876169
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-20 16:28:19 -0400

    LCA question

--- amazon/leetcode_lowest_common_ancestor.py
+++ amazon/leetcode_lowest_common_ancestor.py
@@ -9,4 +9,27 @@ between two nodes p and q as the lowest node in T that has both p and q as
 """
 
 
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+"""
+
+
+"""
+approch -- 
+
+using dfs we get levels ? 
+
+we find both and trace path ? 
+
 """
