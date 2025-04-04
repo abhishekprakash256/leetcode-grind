d90fe35ddb6e5e611bda89853a515b11b990f84b
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-04 14:16:02 -0400

    four sum problem

--- amazon/leetcode_4sum.py
+++ amazon/leetcode_4sum.py
@@ -59,7 +59,7 @@ decision tree but with memo faster ?
 
 
 class Solution_wrong():
-    
+
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         """
         The function to find the four sum of the target sum that is given
@@ -120,3 +120,96 @@ class Solution_wrong():
                         l = m + 1
 
         return results
+
+
+
+from typing import List
+
+
+class Solution():
+    """
+    passes leetcode
+    """
+
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        """
+        The function to find the four sum of the integer that is equal to target given
+        """
+
+
+        #constraint case length is less
+        if len(nums) <= 3 : 
+
+            return []
+
+        #constarint case when length is equal
+        if len(nums) == 4 :
+
+            if sum(nums) == target  :
+
+                return [nums]
+
+        #vars 
+        results = []
+
+        #sort the array
+        nums.sort()
+        
+        #length of nums
+        length = len(nums) 
+
+        for i in range(length - 3) :
+
+            if i > 0 and nums[i] == nums[i-1]:
+
+                continue
+
+            for j in range(i+1,length - 2) :
+
+                if j > i+1 and nums[j] == nums[j-1]:
+
+                    continue
+                #ptrs 
+                l = j + 1 
+                r = length - 1
+
+                #start the binary search
+                while l < r :
+
+                    curr_sum = nums[i] + nums[j] + nums[l] + nums[r]
+
+                    if curr_sum == target :
+
+                        results.append([nums[i],nums[j],nums[l],nums[r]])
+
+                        #skip the duplicates 
+                        while l < r and nums[l] == nums[l+1]:
+
+                            l += 1
+
+                        while l < r and nums[r] == nums[r-1]:
+
+                            r -= 1
+
+                        l += 1
+                        r -= 1
+
+                    elif curr_sum < target :
+
+                        l += 1 
+
+                    else :
+
+                        r -= 1
+
+        return results
+
+
+
+
+
+
+
+
+
+
