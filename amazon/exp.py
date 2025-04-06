14dcc58e15013f02b4e158dd3cf0770b3355dd76
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-05 22:06:02 -0400

    backtrack

--- amazon/exp.py
+++ amazon/exp.py
@@ -5,13 +5,38 @@ Make the susbsets with two approaches
 
 
 
+nums = [1,2,3]
+
 def subsets(i, path):
+    
     if i == len(nums):
+        
         print(path)
-        return
+        
+        return None
+
     subsets(i + 1, path + [nums[i]])  # Include nums[i]
     subsets(i + 1, path)              # Exclude nums[i]
 
 
-print(subsets(2,[1,2]))
+subsets(0, [])  # start from index 0 with empty subset
+
+
+
+
+def backtrack(start, path):
+
+	print(path)
+
+	for i in range(start , len(nums)) :
+
+		path.append(nums[i])
+
+		backtrack(i+1,path)
+
+		path.pop()
+
+
+
+backtrack(0,[])
 
