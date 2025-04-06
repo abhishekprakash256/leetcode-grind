1f27b38c751401b6f505960ae79a550c471453d7
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-05 22:54:04 -0400

    backtracking

--- amazon/exp.py
+++ amazon/exp.py
@@ -5,38 +5,118 @@ Make the susbsets with two approaches
 
 
 
-nums = [1,2,3]
-
-def subsets(i, path):
-    
-    if i == len(nums):
-        
-        print(path)
-        
-        return None
-
-    subsets(i + 1, path + [nums[i]])  # Include nums[i]
-    subsets(i + 1, path)              # Exclude nums[i]
-
-
-subsets(0, [])  # start from index 0 with empty subset
 
 
 
 
-def backtrack(start, path):
-
-	print(path)
-
-	for i in range(start , len(nums)) :
-
-		path.append(nums[i])
-
-		backtrack(i+1,path)
-
-		path.pop()
 
 
+class Solution_recursion():
 
-backtrack(0,[])
+	def __init__(self):
 
+		self.results = []
+
+	def _helper_dfs(self,idx,temp_lst):
+		"""
+		The helper function to make the combinations
+		"""
+		#append in list
+		self.results.append(temp_lst)
+
+		#base case
+
+		for i in range(idx,len(self.nums)) :
+
+			self._helper_dfs(i + 1 , temp_lst + [self.nums[i]])
+		
+
+
+	def main_fun(self,nums):
+		"""
+		The main function to make the combinations lists
+		"""
+
+		self.nums = nums
+
+		#conbstraint case 
+		if len(self.nums) == 0 :
+			return self.results
+
+		temp_lst = []
+		idx = 0 
+
+		self._helper_dfs(0,[])
+
+		return self.results
+
+
+
+class Solution_backtracking():
+
+	def __init__(self):
+
+		self.results = []
+
+	def _helper_dfs(self,idx,temp_lst):
+		"""
+		The helper function to make the combinations
+		"""
+		#append in list
+		self.results.append(temp_lst[:])  #make a copy here
+
+		#base case
+
+		for i in range(idx,len(self.nums)) :
+
+			temp_lst.append(self.nums[i])
+
+			self._helper_dfs(i + 1 , temp_lst)
+
+			temp_lst.pop()
+		
+
+
+	def main_fun(self,nums):
+		"""
+		The main function to make the combinations lists
+		"""
+
+		self.nums = nums
+
+		#conbstraint case 
+		if len(self.nums) == 0 :
+			return self.results
+
+		temp_lst = []
+		idx = 0 
+
+		self._helper_dfs(0,[])
+
+		return self.results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test_arr = [1,2,3]
+
+sol_rec = Solution_recursion()
+
+sol_back = Solution_backtracking()
+
+print(sol_rec.main_fun(test_arr))
+
+print(sol_back.main_fun(test_arr))
