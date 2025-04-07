2f09bd28618972f2e0c6a7fd92bc2cfc9abc6db1
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-07 16:18:41 -0400

    permutations

--- backtracking/leet_code_permutation.py
+++ backtracking/leet_code_permutation.py
@@ -172,23 +172,34 @@ class Solution:
 
 
 
+class Solution:
+    """
+    faster solution using statelesness
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+                    backtrack(path, used)
+                    path.pop()
+                    used[i] = False
+
+        result = []
+        used = [False] * len(nums)
+        backtrack([], used)
+        return result
+
 
 
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
