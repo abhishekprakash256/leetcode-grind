f7a035915b9f2ed08d08bcf9533237a6bca2a90d
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-07 16:14:10 -0400

    permutations

--- backtracking/leet_code_permutation.py
+++ backtracking/leet_code_permutation.py
@@ -90,6 +90,116 @@ class Solution():
 
 
 
+
+
+
+
+
+"""
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+"""
+
+
+
+class Solution:
+    """
+    passees leetcode
+    """
+    def __init__(self):
+
+        self.results = []
+
+    def _helper_dfs(self,permutations) :
+        """
+        The function to make the dfs calls for permutations
+        """
+
+        #base case 
+        if len(permutations) == len(self.nums) :
+
+            self.results.append(permutations[:])
+
+            return
+
+        #make the recursion calls
+        for i in range(len(self.nums)):
+
+            #check if the value is there
+            if self.nums[i] in set(permutations):
+
+                continue
+
+            #add the value 
+            permutations.append(self.nums[i])
+
+            #recusive calls
+            self._helper_dfs(permutations)
+
+            permutations.pop()
+
+
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        The function to find all the possible permutations 
+        """
+
+        self.nums = nums 
+
+        #constraint case 
+        if len(self.nums) == 1 :
+
+            return [self.nums]
+
+        #vars
+        permutations = []
+
+        #make the recursion calls 
+        self._helper_dfs(permutations)
+
+        #return the results
+        return self.results
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     sol = Solution()
     print(sol.permute(nums))
