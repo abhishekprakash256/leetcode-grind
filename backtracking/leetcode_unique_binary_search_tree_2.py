e11d246db85f7510c7d44473ec89a9b6f6e75869
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-17 14:38:58 -0400

    unique binary tree

---
+++ backtracking/leetcode_unique_binary_search_tree_2.py
@@ -0,0 +1,77 @@
+"""
+Given an integer n, return all the structurally unique BST's (binary search trees), 
+which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+"""
+
+
+"""
+
+Example 1:
+
+Input: n = 3
+Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+Example 2:
+
+Input: n = 1
+Output: [[1]]
+
+ 
+
+Constraints:
+
+1 <= n <= 8
+
+"""
+
+
+"""
+
+apprroch -- 
+
+using a recurisive dfs soln to make tree 
+
+helper traversal functuin to check the base case and then append the tree 
+
+make a tree class as well and add the nodes 
+
+have a head and if then add the other left or right 
+
+have the decision to left and right 
+
+either can go null
+
+
+"""
+
+
+class TreeNode:
+	def __init__(self, val=0, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+
+class Solution():
+
+	def __init__(self):
+
+		self.results = []
+
+
+	def _helper_dfs(self,curr_node,left_node,right_node):
+		"""
+		The function to do the helper dfs calls for making tree
+		"""
+		
+
+
+
+
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+    	"""
+		The function to make the possible trees
+    	"""
+
+    	pass
+
