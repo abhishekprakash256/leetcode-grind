bfbaaa81903b68cfb6cdbfc62dc1a36b099757e9
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-17 23:48:31 -0400

    combinations sum III

--- backtracking/leetcode_combination_sum_3.py
+++ backtracking/leetcode_combination_sum_3.py
@@ -46,7 +46,81 @@ Constraints:
 """
 approach -- 
 
+using a list 
+
+append the number to make the sum and check the list length and then equal append the number 
+
+dfs function have list and idx carried
 
 
 
 """
+
+from typing import List
+
+
+
+class Solution():
+    """
+    passes leetcode 
+    """
+
+    def __init__(self):
+
+        self.results = []
+
+    def _helper_dfs(self,idx,sum_lst, curr_sum):
+        """
+        The function to make dfs call for helper function
+        """
+
+        #base case 
+        if len(sum_lst) == self.k :
+
+            if curr_sum == self.n :
+
+                self.results.append(sum_lst[:])
+
+            return
+        
+        if curr_sum > self.n :
+
+            return
+
+        #make the recursive calls
+        for i in range(idx, 10) :
+
+            sum_lst.append(i)
+
+            self._helper_dfs(i + 1, sum_lst , curr_sum + i)
+
+            #pop the used element
+            sum_lst.pop()
+        
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        """
+        The function to make the combinatiosn that have target sum
+        """
+
+        self.n = n 
+        self.k = k
+
+        #vars 
+        sum_lst = []
+        idx = 1
+        curr_sum = 0 
+
+        #make the helper call
+        self._helper_dfs(idx, sum_lst , curr_sum)
+
+        return self.results
+
+
+
+
+sol = Solution()
+
+res = sol.combinationSum3(k=3,n=9)
+
+print(res)
