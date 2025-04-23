9d5041d85a2b6af7f3f60d312373d127a68fa250
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-23 18:43:03 -0400

    graph count

--- graphs/leetcode_connected_components_undirected_graph.py
+++ graphs/leetcode_connected_components_undirected_graph.py
@@ -44,80 +44,215 @@ from collections import defaultdict
 
 
 class Solution():
-	"""
-	passes leetcode
-	"""
+    """
+    passes leetcode
+    """
 
-	def __init__(self):
+    def __init__(self):
 
-		self.graph = defaultdict(list)
+        self.graph = defaultdict(list)
 
-		self.visited = set()
-
-
-	def make_graph(self,edges):
-		"""
-		The function to make the graph
-		"""
-
-		for a, b in edges :
-
-			self.graph[a].append(b)
-			self.graph[b].append(a)
+        self.visited = set()
 
 
-	def helper_dfs(self,node):
-		"""
-		The function to make the helper dfs
-		"""
-		#base case 
+    def make_graph(self,edges):
+        """
+        The function to make the graph
+        """
 
-		if node in self.visited :
-			
-			return
+        for a, b in edges :
 
-		#print the node 
-		#print(node)
-
-		#add in the visited 
-		self.visited.add(node)
-
-		for neighbor in self.graph[node]:
-
-			self.helper_dfs(neighbor)
-		
+            self.graph[a].append(b)
+            self.graph[b].append(a)
 
 
-	def countComponents(self, n , edges) :
-		"""
-		The function to count the number of connected edges 
-		"""
+    def helper_dfs(self,node):
+        """
+        The function to make the helper dfs
+        """
+        #base case 
 
-		#constraints case 
+        if node in self.visited :
+            
+            return
 
-		#if one node 
-		if n == 1 :
+        #print the node 
+        #print(node)
 
-			return 1
+        #add in the visited 
+        self.visited.add(node)
 
-		#make the graph
-		self.make_graph(edges)
+        for neighbor in self.graph[node]:
 
-		#print(self.graph)
+            self.helper_dfs(neighbor)
+        
 
-		#count vars 
-		count = 0 
 
-		#do the recursive traversal 
-		for node in range(n) :
+    def countComponents(self, n , edges) :
+        """
+        The function to count the number of connected edges 
+        """
 
-			if node not in self.visited :
+        #constraints case 
 
-				count +=1 
+        #if one node 
+        if n == 1 :
 
-				self.helper_dfs(node)
+            return 1
 
-		return count
+        #make the graph
+        self.make_graph(edges)
+
+        #print(self.graph)
+
+        #count vars 
+        count = 0 
+
+        #do the recursive traversal 
+        for node in range(n) :
+
+            if node not in self.visited :
+
+                count +=1 
+
+                self.helper_dfs(node)
+
+        return count
+
+
+
+
+
+class Solution():
+
+    def __init__(self):
+
+        self.graph = defaultdict(list)
+        self.visited = set()
+        self.count += 1 
+
+
+    def _make_graph(self,edges):
+        """
+        The function to make the graph by edges list
+        """
+
+        for u,v in edges :
+
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+
+
+    def _helper_dfs(self,node):
+        """
+        The function to traverese the graph in dfs 
+        """
+
+        #base case
+        if node in self.visited :
+
+            return
+
+        #add the node
+        self.visited.add(node)
+
+        #travere the graph
+        for neighbor in self.graph[node] :
+
+            if neighbor not in self.visited :
+
+                self._helper_dfs(neighbor)
+
+
+
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        """
+        The function to find the number of number of connected components
+        """
+
+        #constraint case 
+
+        if n == 1 :
+
+            return 1
+
+        #make the graph
+        self._make_graph(edges)
+
+        #traverese the graph
+        for node in range(n):
+
+            if node not in self.visited :
+
+                self._helper_dfs(node)
+                self.count += 1 
+
+        #return the count
+        return self.count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
