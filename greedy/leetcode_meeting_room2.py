87842d440b1c3dbada561ebb19306c7a9405842a
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-04-28 21:08:10 -0400

    meeting room

--- greedy/leetcode_meeting_room2.py
+++ greedy/leetcode_meeting_room2.py
@@ -27,10 +27,62 @@ Constraints:
 
 
 """
-
 approach --
 
+min = 0 
+max = 30
+
+sort the array using the first val
+
+meeting room we need is 1 
+
+last max and next min 
+
+room = 1 
+
+max > min
+
+    room += 1
 
 
+rooms = [[9,10],[4,9],[4,17]]
+
+sorted_rooms = [[4,9],[4,17],[9,10]] 
 
 """
+
+from typing import List
+
+class Solution_Wrong:
+    """
+    Wrong Solution
+    """
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        """
+        The function to find the meeting rooms needed
+        """
+
+        #constraint case 
+        if len(intervals) == 1:
+
+            return 1
+
+        #vars 
+        room = 1
+
+        #sort the list 
+        intervals.sort() 
+
+        #traverse the list 
+        for i in range(1, len(intervals)) :
+
+            if intervals[1][i-1] > intervals[i][0] :
+
+                room += 1 
+
+        return room
+
+
+
+
+        
