d6149e9bc4ff984927583bdf8994611caa6825bb
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-08-12 23:56:48 -0400

    uber question

--- Uber/leetcode_my_calendar.py
+++ Uber/leetcode_my_calendar.py
@@ -86,6 +86,30 @@ class MyCalendar:
 
 		
 
+from sortedcontainers import SortedList
+
+class MyCalendar:
+	"""
+	The optim soln
+	"""
+    def __init__(self):
+        self.start_events = SortedList()
+        self.end_events = SortedList()
+
+    def book(self, start: int, end: int) -> bool:
+        left = bisect_right(self.end_events, start)
+        right = bisect_left(self.start_events, end)
+        if left == right:
+            # add book
+            self.start_events.add(start)
+            self.end_events.add(end)
+            return True
+        else: return False
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
