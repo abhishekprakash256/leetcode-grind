f78e1e19d4488beee6d1b7b0b24819946e029d4f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-08-18 22:55:48 -0400

    uber code

--- Uber/leetcode_first_unique.py
+++ Uber/leetcode_first_unique.py
@@ -3,9 +3,9 @@ You have a queue of integers, you need to retrieve the first unique integer in t
 
 Implement the FirstUnique class:
 
-    FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
-    int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
-    void add(int value) insert value to the queue.
+	FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
+	int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
+	void add(int value) insert value to the queue.
 
 """
 
@@ -34,8 +34,81 @@ firstUnique.showFirstUnique(); // return -1
 
 """
 
+approach can be using a counter of dict 
 
+when get the data store in the collection counter 
 
+and get the uinique value which comes 1 times and make as unique
 
+when we add the value match with with unique value and update the collection counter as well 
+
+if multplile unique we have move the pointer one step back to the new value
 
 """
+
+from collections import Counter
+
+
+class FirstUnique_wrong:
+
+	def __init__(self, nums: List[int]):
+
+		self.nums = nums
+
+		self.mapper = Counter(self.nums)
+
+		self.unique_lst = []
+
+		for key in self.mapper:
+
+			if self.mapper[key] == 1 :
+
+				self.unique_lst.append(key)
+
+		
+
+	def showFirstUnique(self) -> int:
+		"""
+		The function to get the first value
+		"""
+
+		if len(self.unique_lst) > 0 :
+
+			left_val = self.unique_lst.pop()
+
+			self.unique_lst.insert(0,left_val)
+
+			return left_val
+
+		return -1
+
+
+
+		
+
+	def add(self, value: int) -> None:
+		"""
+		The function to add into the queue
+		"""
+
+		self.nums.append(value)
+
+		self.mapper = Counter(self.nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
