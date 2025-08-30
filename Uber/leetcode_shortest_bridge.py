86e80449071e84e69beacd21d8f3042f55056d1f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-08-30 18:30:58 -0400

    shortest code

--- Uber/leetcode_shortest_bridge.py
+++ Uber/leetcode_shortest_bridge.py
@@ -28,10 +28,10 @@ Output: 1
 
 Constraints:
 
-    n == grid.length == grid[i].length
-    2 <= n <= 100
-    grid[i][j] is either 0 or 1.
-    There are exactly two islands in grid.
+	n == grid.length == grid[i].length
+	2 <= n <= 100
+	grid[i][j] is either 0 or 1.
+	There are exactly two islands in grid.
 
 
 
@@ -70,9 +70,9 @@ second_island_edge = []
 
 def dfs(i,j,edge_lst):
 
-    all edge cond 
-    
-    if 
+	all edge cond 
+	
+	if 
 
 
 
@@ -83,19 +83,135 @@ count = 0
 
 for i in range row:
 
-    for j in range col :
+	for j in range col :
 
-        if matix[i][j] == 1 :
+		if matix[i][j] == 1 :
 
-            first_island_edges.append([i,j])
-            self.dfs(i,j , first_island_edges)
+			first_island_edges.append([i,j])
+			self.dfs(i,j , first_island_edges)
 
-            count += 1 
+			count += 1 
 
 
 
 
 """
+
+from typing import List
+from collections import deque
+
+
+
+class Solution:
+
+	def dfs(self,i,j):
+		"""
+		The method to find the complete island
+		"""
+
+		#constraint condn
+		if i < 0 or i >= self.row or j < 0 or j >= self.col or self.grid[i][j] == 0:
+			
+			return
+
+		#make the point as 2
+		self.grid[i][j] = 2
+
+		#move into the dirs
+		self.dfs(i-1,j)
+		self.dfs(i+1,j)
+		self.dfs(i,j+1)
+		self.dfs(i,j-1)
+
+
+
+	def shortestBridge(self, grid: List[List[int]]) -> int:
+		"""
+		The function to find the shortest bridge
+		"""
+
+		self.grid = grid
+		self.row = len(self.grid)
+		self.col = len(self.grid[0])
+
+		#the queue for bfs
+		queue = deque()
+
+		#directiosn to move
+		dirs = [[0,1],[0,-1],[-1,0],[1,0]]
+
+		#the visited dict
+		visited = {}
+
+		#distance 
+		dist = 0
+
+
+		#iter and find the first one 
+		for i in range(self.row) :
+
+			for j in range(self.col):
+
+				#find the grid value 1
+				if self.grid[i][j] == 1 :
+
+					#pass into the dfs
+					self.dfs(i,j)
+
+					queue.append([i,j])
+
+					break
+
+		#find the shortest distance
+		while queue :
+
+			#pop from the queue
+			x , y = queue.popleft()
+
+			#move in the dirs
+			for dir_x, dir_y in dirs :
+
+				nx = x + dir_x
+				ny = y + dir_y
+
+				#catch the visited
+				if (nx,ny) not in visited or self.grid[nx][ny] != 2 or x >= 0 or x <= self.row or ny >= 0 or ny <= self.col :
+
+					queue.append([nx,ny])
+
+					visited[(nx,ny)] = True
+
+					dist += 1
+
+					print(nx,ny)
+
+					if self.grid[nx][ny] == 1 :
+
+						return dist
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	sol = Solution()
+
+	grid = [[0,1,0],[0,0,0],[0,0,1]]
+
+	res = sol.shortestBridge(grid)
+
+	print(res)
+
+
+
+
+
+
+
+		
+		
 
 
 
