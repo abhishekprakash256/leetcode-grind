2e234ad4b7fed91833ac04de818407d4feacb2b6
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-03-06 16:57:47 -0500

    prefix sum

--- 2026/prefix-sum/basic_prefix_sum.py
+++ 2026/prefix-sum/basic_prefix_sum.py
@@ -1,3 +1,22 @@
 """
 The basics prefix sum 
 """
+
+class Solution():
+
+	def prefix_sum(self, nums):
+
+		prefix = [0] * len(nums)
+
+		for i in range(1,len(nums)):
+
+			prefix[i] = prefix[i-1] + nums[i]
+
+		return prefix
+
+
+sol = Solution()
+
+nums = [2,4,1,7,3]
+
+print(sol.prefix_sum(nums))
