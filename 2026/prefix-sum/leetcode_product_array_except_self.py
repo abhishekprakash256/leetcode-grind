167f46d8b19cb481a6434d9b6afb9a3eb5048e51
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-03-10 00:47:18 -0400

    wrong code

--- 2026/prefix-sum/leetcode_product_array_except_self.py
+++ 2026/prefix-sum/leetcode_product_array_except_self.py
@@ -22,9 +22,9 @@ Output: [0,0,9,0,0]
 
 Constraints:
 
-    2 <= nums.length <= 105
-    -30 <= nums[i] <= 30
-    The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+	2 <= nums.length <= 105
+	-30 <= nums[i] <= 30
+	The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
 
  
 
@@ -34,6 +34,82 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
 
 """
 
+approach -- 
+
+make a mutiply array 
+
+from left and right 
+
+two arrays 
+
+then multiply the elemnts to get the value on the spot 
+
+from one spot left and right 
+
+
+if 0 then , right_prefix [0,len(arra)]
+
+if idx - 1 < 0 
+	
+	igone left 
+
+if idx + 1 > len(arr) -1 :
+
+	ignore right
 
 
 """
+from typing import List
+
+class Solution:
+	def productExceptSelf(self, nums: List[int]) -> List[int]:
+		"""
+		The function to find the prefix multiplication array
+		"""
+
+		#arry list
+		left_prefix = [1] * (len(nums)) 
+
+		right_prefix = [1] * (len(nums))
+
+		ans = [1] * len(nums)
+
+		#make the prefix array
+		for i in range(len(nums)) :
+
+			left_prefix[i] = left_prefix[i-1] * nums[i]
+
+
+		for i in range(len(nums) , 0) :
+
+			right_prefix[i] = right_prefix[i + 1] * nums[i]
+
+
+		for i in range(len(nums)) :
+
+			if i - 1 < 0 :
+
+				ans[i] = ans[i] * right_prefix[i + 1]
+
+			if i + 1 > len(nums) - 1:
+
+				ans[i] = ans[i] * left_prefix[ i - 1]
+
+			else :
+
+				ans[i] = left_prefix[i-1] * right_prefix[i + 1]
+
+
+		return ans
+
+
+
+if __name__ == "__main__" :
+
+	sol = Solution()
+
+	nums = [1,2,3,4]
+
+	res = sol.productExceptSelf(nums)
+
+	print(res)
