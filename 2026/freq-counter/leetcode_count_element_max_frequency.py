4696d3ad6a0fb1e4dd1a745593fd104a5e519072
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-04-15 16:13:33 -0400

    freq counter

---
+++ 2026/freq-counter/leetcode_count_element_max_frequency.py
@@ -0,0 +1,77 @@
+"""
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+"""
+
+"""
+
+
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+
+ 
+
+Constraints:
+
+	1 <= nums.length <= 100
+	1 <= nums[i] <= 100
+
+
+"""
+
+"""
+approach -- 
+
+from collections import Counter
+
+freq = Coubnter(nums)
+
+freq.value.sort()
+
+first_key = next(iter(freq))
+
+
+"""
+
+from collections import Counter
+
+
+
+
+
+
+from collections import Counter
+
+from typing import List
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        
+        freq = Counter(nums)
+
+        max_freq = max(freq.values())
+
+        result = 0
+        
+        for val in freq.values():
+            if val == max_freq:
+                result += val
+
+        return result
+
+
+
