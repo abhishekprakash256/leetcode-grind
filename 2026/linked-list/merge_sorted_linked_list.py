fb9de057edc9fc76c19bdf820bd2d0c8bd97633a
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-04-22 22:23:35 -0400

    merge code

--- 2026/linked-list/merge_sorted_linked_list.py
+++ 2026/linked-list/merge_sorted_linked_list.py
@@ -91,6 +91,35 @@ class LinkedListHelper():
 
 
 
+	def merge_two_list(self, l1, l2):
+		"""
+		The function to merge the list 
+
+		"""
+
+		if not l1 :
+
+			return l2
+
+		if not l2 :
+
+			return l1
+
+		if l1.val < l2.val :
+
+			l1.next = merge_two_list(l1.next , l2)
+
+			return l1
+
+
+		else:
+
+			l2.next = merge_two_list(l1, l2.next)
+
+			return l2
+
+
+
 	def print_link_list(self,head):
 
 		curr = head
