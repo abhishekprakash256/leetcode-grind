573005a0029d5cdbd80df01b6bf4e828a5fcbc35
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-07-13 22:21:25 -0400

    code

---
+++ 2026/recursion/combination.py
@@ -0,0 +1,30 @@
+"""
+basic template for the combinations
+"""
+
+
+res = []
+path = []
+
+nums = [4]
+
+def dfs(i):
+
+    if i == len(nums):
+        res.append(path[:])
+        return
+
+    # take
+    path.append(nums[i])
+    dfs(i + 1)
+
+    # undo
+    path.pop()
+
+    # skip
+    dfs(i + 1)
+
+
+dfs(1)
+
+print(res)
