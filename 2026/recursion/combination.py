1236ba570fcf5fb3be2a1ddb12a4cfc4ec96a33a
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-07-18 21:16:15 -0400

    leetcode combinations

--- 2026/recursion/combination.py
+++ 2026/recursion/combination.py
@@ -28,3 +28,42 @@ def dfs(i):
 dfs(0)
 
 print(res)
+
+
+
+
+class Solution():
+
+    def __init__(self):
+
+        self.path = []
+        self.res  = []
+
+    def dfs(self,i, nums):
+        """
+        The function to find the combinations
+        """
+
+        if i == len(nums): 
+
+            self.res.append(self.path[:])
+            return
+
+        #take
+        self.path.append(nums[i])
+        self.dfs(i+1 , nums)
+
+        # undo 
+        self.path.pop()
+
+        #skip
+        self.dfs(i+1 , nums)
+
+
+
+sol = Solution()
+
+
+sol.dfs(0 , nums)
+
+print(sol.res)
