c9164c3550f3357c926e6ea0da00bddad44a4db8
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-08-03 22:30:57 -0400

    another solution

--- 2026/recursion/permutations.py
+++ 2026/recursion/permutations.py
@@ -65,6 +65,62 @@ class Solution():
 
 
 
+
+
+
+
+class Solution():
+
+	def __init__(self):
+		self.res_lst = []
+
+
+	def helper(self,start_lst):
+		"""
+		The helper funciton
+		"""
+
+		#base case 
+		if len(start_lst) == len(self.nums):
+			self.res_lst.append(start_lst)
+			return None
+
+
+		for i in self.nums:
+
+			if i in start_lst:
+
+				continue
+
+			#start_lst.append(i)
+
+			self.helper(start_lst + [i])
+
+			#start_lst.pop()
+
+
+	def permute(self,nums):
+		"""
+		The permute function
+		"""
+
+		self.nums = nums
+
+
+		#base case
+		if len(nums) == 1:
+			return [nums]
+
+		start_lst = []
+
+		self.helper(start_lst)
+
+
+		return self.res_lst
+
+
+
+
 sol = Solution()
 
 
