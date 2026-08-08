efe230a9c091f738dc9db079beec74154fb9664a
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-08-08 13:32:21 -0400

    leetcode_generate_paranthesis.py

---
+++ 2026/recursion/leetcode_generate_paranthesis.py
@@ -0,0 +1,106 @@
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+"""
+
+"""
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+
+Constraints:
+
+1 <= n <= 8
+
+
+"""
+
+
+"""
+approach -- 
+
+using the approch , left and right and balance,
+
+if right is open then discard, if left is open then discard
+
+
+"""
+
+
+
+
+class Solution:
+	def __init__(self):
+
+		self.res = []
+		self.para_lst = ["(",")"]
+
+
+	def _helper(self,i, para_str):
+		"""
+		The function to make the dfs
+		"""
+
+		#discard case
+		if para_str[2*self.n -1 ] == "(" :
+
+			return
+
+
+		#make the base case
+		if i*2 == len(para_str) :
+
+			self.res.append(para_str)
+
+			return
+
+
+		#make the stack
+		for para in self.para_lst:
+
+			self._helper(i+1, para_str + para)
+
+
+
+
+	def generateParenthesis(self, n: int) -> List[str]:
+		"""
+		The function to make the paranthessis
+		"""
+
+		self.n = n
+
+		#make the str
+		para_str = "("
+
+		#call the helper
+		self._helper(0, para_str )
+
+		return self.res
+
+
+
+
+#testing the solution 
+
+
+
+if __name__ == '__main__':
+
+	sol = Solution()
+
+	res = sol.generateParenthesis(3)
+
+	print(res)
+
+
+
+
+
+
